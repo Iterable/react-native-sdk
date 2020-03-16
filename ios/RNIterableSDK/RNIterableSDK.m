15b4bdea206a4b98738614391d2bade8e7c9ca70
Tapash Majumder <tapash@iterable.com>
2020-03-16 12:21:42 +0530

    Add callback tester getMessages method.

--- ios/RNIterableSDK/RNIterableSDK.m
+++ ios/RNIterableSDK/RNIterableSDK.m
@@ -7,6 +7,18 @@
 
 RCT_EXPORT_MODULE();
 
+RCT_EXPORT_METHOD(initializeWithApiKey: (NSString *) apiKey) {
+    RCTLogInfo(@"IterableAPI.initialize(:apiKey)");
+    [IterableAPI initializeWithApiKey: apiKey];
+}
+
+RCT_REMAP_METHOD(getMessages,
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject) {
+    NSArray* messages =  [[IterableAPI inAppManager] getMessages];
+    resolve(messages);
+}
+
 RCT_EXPORT_METHOD(setEmail: (NSString *) email) {
     IterableAPI.email = email;
     RCTLogInfo(@"Setting Iterable email %@", email);
