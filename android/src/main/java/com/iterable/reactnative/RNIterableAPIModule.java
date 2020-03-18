6c3b7bac2f337a0ebdb691baa81c316e19c09acf
Tapash Majumder <tapash@iterable.com>
2020-03-18 20:33:07 +0530

    Fix Bridge class

--- android/src/main/java/com/iterable/reactnative/RNIterableAPIModule.java
+++ android/src/main/java/com/iterable/reactnative/RNIterableAPIModule.java
@@ -1,9 +1,13 @@
 package com.iterable.reactnative;
 
+import android.util.Log;
+
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.iterable.iterableapi.IterableApi;
 
 public class RNIterableAPIModule extends ReactContextBaseJavaModule {
 
@@ -20,16 +24,24 @@ public class RNIterableAPIModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void initializeApiWithKey(String key) {
+    public void initializeWithApiKey(String apiKey) {
         //
+        Log.d("ReactNative", "initializeWithApiKey: " + apiKey);
+        IterableApi.getInstance().initialize(reactContext, apiKey);
     }
 
     @ReactMethod
     public void setEmail(String email) {
         //
+        Log.d("ReactNative", "setEmail: " + email);
+        IterableApi.getInstance().setEmail(email);
     }
 
-
+    @ReactMethod
+    public void getInAppMessages(Promise promise) {
+        Log.d("ReactNative", "getMessages");
+        promise.resolve(IterableApi.getInstance().getInAppManager().getMessages());
+    }
 
     @ReactMethod
     public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
