dcb603a60fe170cdb0859bdefe3399d07d696424
Tapash Majumder <tapash@iterable.com>
2020-03-18 11:27:22 +0530

    Add first version of Android sample app.

--- SampleApp/android/app/src/main/java/com/sampleapp/MainActivity.java
+++ SampleApp/android/app/src/main/java/com/sampleapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.myapp;
+package com.sampleapp;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "MyApp";
+    return "SampleApp";
   }
 }
--- SampleApp/android/app/src/main/java/com/sampleapp/MainApplication.java
+++ SampleApp/android/app/src/main/java/com/sampleapp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.myapp;
+package com.sampleapp;
 
 import android.app.Application;
 import android.content.Context;
