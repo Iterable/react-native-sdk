198f1dfd1f0e507fa53d85965cd59184fb2aec07
Tapash Majumder <tapash@iterable.com>
2020-04-16 13:34:33 +0530

    Add tabbar icon

--- SampleApp/src/App.tsx
+++ SampleApp/src/App.tsx
@@ -1,6 +1,7 @@
-import * as React from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons'
 
 import HomeTab from './HomeTab'
 import SettingsTab from './SettingsTab'
@@ -10,7 +11,22 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            if (route.name == 'Home') {
+              return <Icon name="ios-home" size={size} color={color} />
+            } else {
+              return <Icon name="ios-settings" size={size} color={color} />
+            }
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+          showIcon: true,
+        }}
+      >
         <Tab.Screen name="Home" component={HomeTab} />
         <Tab.Screen name="Settings" component={SettingsTab} />
       </Tab.Navigator>
