7881d515c173eaf44b37bb7069024c6fe050aac5
Tapash Majumder <tapash@iterable.com>
2020-04-17 12:13:29 +0530

    Login/logout handling

--- SampleApp/src/SettingsTab.tsx
+++ SampleApp/src/SettingsTab.tsx
@@ -11,38 +11,51 @@ import {
   Image,
 } from 'react-native'
 import { TextInput } from 'react-native-gesture-handler';
+import {Iterable} from 'react-native-iterable'
 
 interface Props {}
 interface State {
-  isLoggedIn: boolean
+  email?: String
 }
 
 class SettingsTab extends Component<Props, State> {
   constructor(props: Props) {
     super(props)
-    this.state = {isLoggedIn: false}
+    this.state = {}
+    this.updateState()
+  }
+
+  updateState() {
+    console.log("updateState")
+    Iterable.getEmail().then(email => {
+      console.log("gotEmail: " + email)
+      if (email) {
+        this.setState((prevState, props) => {return {email: email}})
+      } else {
+        this.setState((prevState, props) => {return {email: undefined}})
+      }
+    })
   }
 
   render() {
-    if (this.state.isLoggedIn == true) {
-      return this.renderLoggedIn()
+    console.log("render")
+    if (this.state.email) {
+      console.log("renderLoggedIn: " + this.state.email)
+      return this.renderLoggedIn(this.state.email)
     } else {
+      console.log("renderLoggedOut")
       return this.renderLoggedOut()
     }
   }
 
-  renderLoggedIn() {
+  renderLoggedIn(email: String) {
+    console.log("email: " + email)
     return (
       <View style={styles.emailContainer}>
-        <Text>Hello, World</Text>
-        <TextInput 
-          style={styles.emailTextInput} 
-          autoCapitalize="none" 
-          autoCompleteType="email"
-          placeholder="user@example.com" />
+        <Text>{email}</Text>
         <Button
-          title="Login"
-          onPress={() => {this.setState({isLoggedIn: false})}}
+          title="Logout"
+          onPress={this.onLogoutTapped}
         />
       </View>
    ) 
@@ -65,7 +78,15 @@ class SettingsTab extends Component<Props, State> {
   }
 
   onLoginTapped = () => {
-    this.setState({isLoggedIn: true})
+    console.log("onLoginTapped")
+    Iterable.setEmail("tapash@iterable.com")
+    this.updateState()
+  }
+
+  onLogoutTapped = () => {
+    console.log("onLogoutTapped")
+    Iterable.setEmail(null)
+    this.updateState()
   }
 }
 
