76f4887dd9edf56542db6c55981516c10a01f5b4
Tapash Majumder <tapash@iterable.com>
2020-04-18 13:17:04 +0530

    Change bundle identifier

--- SampleApp/ios/SampleApp/File.swift
+++ SampleApp/ios/SampleApp/File.swift
@@ -1,9 +1,6 @@
 //
-//  File.swift
-//  MyApp
-//
 //  Created by Tapash Majumder on 2/27/20.
 //  Copyright © 2020 Facebook. All rights reserved.
-//
+//  This blank file is needed so that we can use swift frameworks in our ObjC project.
 
 import Foundation
