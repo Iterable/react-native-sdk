60aadc0d73e13a5f9606ece8612705cdf6a921fd
Tapash Majumder <tapash@iterable.com>
2020-04-18 14:38:03 +0530

    1. Add notification extension.

---
+++ SampleApp/ios/Rich Notification Extension/NotificationService.swift
@@ -0,0 +1,11 @@
+//
+//  Created by Tapash Majumder on 4/18/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import UserNotifications
+
+import IterableAppExtensions
+
+class NotificationService: ITBNotificationServiceExtension {
+}
