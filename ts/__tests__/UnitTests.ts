2b85a321ababd959e2814d8b7deea5c37675060e
Tapash Majumder <tapash@iterable.com>
2020-06-01 20:56:33 +0530

    remove unneeded test

--- ts/__tests__/UnitTests.ts
+++ ts/__tests__/UnitTests.ts
@@ -1,9 +1,5 @@
 import { Iterable } from '../Iterable'
 
-test('a', () => {
-  expect(1 + 2).toBe(3)
-})
-
 test("set/get email", () => {
   Iterable.setEmail("user@example.com")
   return Iterable.getEmail().then(email => {
