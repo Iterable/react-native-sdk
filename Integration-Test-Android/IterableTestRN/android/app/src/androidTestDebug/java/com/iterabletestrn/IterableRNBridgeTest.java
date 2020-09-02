c0536d7b3571d0c841855ea0ac57455911c33e01
Akshay Ayyanchira <ayyanchira.akshay@gmail.com>
2020-09-02 14:12:15 -0700

    [MOB-1812] - Android Integration Test via Espresso
    Base project for RN Android Integration tests using Espresso.
    Use Espresso Recorder tool in Android Studio to create new test methods. It will generate a new class.
    Copy the Iterable related test methods to IterableRNBridgeTest class.
    Keep app specific UI test in AppLayoutTest class.

---
+++ Integration-Test-Android/IterableTestRN/android/app/src/androidTestDebug/java/com/iterabletestrn/IterableRNBridgeTest.java
@@ -0,0 +1,40 @@
+package com.iterabletestrn;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.AfterClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class IterableRNBridgeTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+
+}
---
+++ Integration-Test-Android/IterableTestRN/android/app/src/main/java/com/iterabletestrn/IterableTestPackage.java
@@ -0,0 +1,29 @@
+package com.iterabletestrn;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class IterableTestPackage implements ReactPackage {
+    @NonNull
+    @Override
+    public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+        //this is where you register the module
+        modules.add(new TestBridge(reactContext));
+        return modules;
+    }
+
+    @NonNull
+    @Override
+    public List<ViewManager> createViewManagers(@NonNull ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
