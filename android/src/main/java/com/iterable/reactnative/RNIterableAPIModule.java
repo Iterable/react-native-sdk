2f7a2455078d2bbeba139204c182ef6d543fe02e
EricH <AvarioDeveloper@gmail.com>
2020-10-01 16:53:47 -0600

    Always tell android-sdk url and action not handled

--- android/src/main/java/com/iterable/reactnative/RNIterableAPIModule.java
+++ android/src/main/java/com/iterable/reactnative/RNIterableAPIModule.java
@@ -348,7 +348,8 @@ public class RNIterableAPIModule extends ReactContextBaseJavaModule implements I
         } catch (JSONException e) {
             IterableLogger.e(TAG, "Failed handling custom action");
         }
-        return true;
+        // The Android SDK will not bring the app into focus is this is `true`. It still respects the `openApp` bool flag.
+        return false;
     }
 
     @NonNull
@@ -386,7 +387,8 @@ public class RNIterableAPIModule extends ReactContextBaseJavaModule implements I
         } catch (JSONException e) {
             IterableLogger.e(TAG, e.getLocalizedMessage());
         }
-        return true;
+        // The Android SDK will not bring the app into focus is this is `true`. It still respects the `openApp` bool flag.
+        return false;
     }
 
     // ---------------------------------------------------------------------------------------
