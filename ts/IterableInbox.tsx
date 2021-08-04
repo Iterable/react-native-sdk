a47326decf662351394a28282086488b50a88b2b
Evan Greer <evan.greer@iterable.com>
2021-08-04 16:40:11 -0600

    adds message display component and associated inbox functionality

--- ts/IterableInbox.tsx
+++ ts/IterableInbox.tsx
@@ -3,20 +3,37 @@ import React, { Component, useState } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import IterableInboxMessageList from './IterableInboxMessageList';
 import IterableInboxEmptyState from './IterableInboxEmptyState';
+import IterableInboxMessageDisplay from './IterableInboxMessageDisplay';
 import sampleMessages from './sampleMessageData.js';
 
 const IterableInbox = () => {
    const inboxTitle = "Inbox";
+   const [isDisplayMessage, setIsDisplayMessage] = useState(false);
    const [selectedMessageIdx, setSelectedMessageIdx] = useState(0);
    const [messages, setMessages] = useState(sampleMessages);
+
+   let selectedMessage = messages[selectedMessageIdx];
 
    function handleMessageSelect(index: number, messages: Array<any>) {
       if(!messages[index].read) {
          messages[index].read = true
       }
       setMessages([...messages]);
+      setIsDisplayMessage(true);
       setSelectedMessageIdx(index);
-   }   
+   }
+
+   function returnToInbox() {
+      setIsDisplayMessage(false);
+   }
+   
+   function displayMessage(message: {[key: string]: any}) {
+      return (
+         <IterableInboxMessageDisplay 
+            message={message}
+            returnToInbox={returnToInbox}
+         ></IterableInboxMessageDisplay>)
+   }
 
    function showMessageList() {
       return (
@@ -35,7 +52,7 @@ const IterableInbox = () => {
 
    return(
       <View style={styles.container}>
-         {showMessageList()}
+         {isDisplayMessage ? displayMessage(selectedMessage) : showMessageList()}
       </View>
    )
 }
