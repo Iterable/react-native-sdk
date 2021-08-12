70c4f0284f4f9cf40a577293910d128d261c9eef
Jay Kim <jay@nullavenue.com>
2021-08-12 15:06:57 -0700

    add inbox data model listening

--- ts/IterableInboxDataModel.ts
+++ ts/IterableInboxDataModel.ts
@@ -1,15 +1,31 @@
 'use strict'
 
+import { Component } from 'react'
 import { NativeModules } from 'react-native'
 import { IterableInAppMessage, IterableInAppLocation, IterableInAppDeleteSource, InboxMessageDataModel } from '.'
 
 const RNIterableAPI = NativeModules.RNIterableAPI
 
+interface InboxComponent {
+    updateView: () => void
+}
+
 class IterableInboxDataModel {
     inboxMessages: Array<InboxMessageDataModel> = []
 
-    constructor() {
-        IterableInboxDataModel.syncInboxMessages()
+    private listeners: Set<InboxComponent> = new Set()
+
+    constructor(listener: InboxComponent) {
+        this.addListener(listener)
+        this.syncInboxMessages()
+    }
+
+    private addListener(listener: InboxComponent) {
+        this.listeners.add(listener)
+    }
+
+    private removeListener(listener: InboxComponent) {
+        this.listeners.delete(listener)
     }
 
     getItemCount(): number {
@@ -18,12 +34,16 @@ class IterableInboxDataModel {
         return this.inboxMessages.length
     }
 
+    getAllItems(): Array<InboxMessageDataModel> {
+        return this.inboxMessages
+    }
+
     deleteItem(row: number, deleteSource: IterableInAppDeleteSource) {
         console.log("IterableInboxDataModel.deleteItem")
 
         this.inboxMessages.splice(row, 1)
         RNIterableAPI.removeMessage(this.idForRow(row), IterableInAppLocation.inbox, deleteSource)
-        IterableInboxDataModel.syncInboxMessages()
+        this.syncInboxMessages()
     }
 
     getItem(row: number): InboxMessageDataModel {
@@ -43,14 +63,35 @@ class IterableInboxDataModel {
         return this.inboxMessages[row].inAppMessage.messageId
     }
 
-    private static getDataModelForMessage(message: IterableInAppMessage): InboxMessageDataModel {
-        return new InboxMessageDataModel(message)
+    private syncInboxMessages() {
+        RNIterableAPI.getInboxMessages().then(
+            (messages: Array<IterableInAppMessage>) => {
+                this.inboxMessages = messages.map(IterableInboxDataModel.getDataModelForMessage)
+
+                this.listeners.forEach(
+                    (listener) => {
+                        listener.updateView()
+                    }
+                )
+            }
+        )
     }
 
-    private static syncInboxMessages() {
-        RNIterableAPI.getInboxMessages().then(
-            (messages: Array<IterableInAppMessage>) => messages.map(IterableInboxDataModel.getDataModelForMessage)
-        )
+    //** not sure if this is correct yet */
+    // async refresh(): Promise<Array<InboxMessageDataModel>> {
+    //     return RNIterableAPI.getInboxMessages().then(
+    //         (messages: Array<IterableInAppMessage>) => {
+    //             this.inboxMessages = messages.map(IterableInboxDataModel.getDataModelForMessage)
+    //             return this.inboxMessages
+    //         },
+    //         () => {
+    //             return []
+    //         }
+    //     )
+    // }
+
+    private static getDataModelForMessage(message: IterableInAppMessage): InboxMessageDataModel {
+        return new InboxMessageDataModel(message)
     }
 }
 
