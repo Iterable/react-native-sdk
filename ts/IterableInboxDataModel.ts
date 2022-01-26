b02d5c71b40403bafd47374e3399495b60eaf9d3
Evan Greer <evan.greer@iterable.com>
2022-01-26 16:57:47 -0700

    edits addition to only include type reassignment for android

--- ts/IterableInboxDataModel.ts
+++ ts/IterableInboxDataModel.ts
@@ -113,51 +113,7 @@ class IterableInboxDataModel {
             createdAt = new Date(message.createdAt)
         }
 
-        let hour = createdAt.getHours() > 12 ? createdAt.getHours() - 12 : createdAt.getHours()
-        let AMPM = createdAt.getHours() > 12 ? 'PM' : 'AM'
-        let monthStr
-        switch (createdAt.getMonth()) {
-            case 0:
-                monthStr = 'Jan'
-                break
-            case 1:
-                monthStr = 'Feb'
-                break  
-            case 2:
-                monthStr = 'Mar';
-                break
-            case 3:
-                monthStr = 'Apr';
-                break
-            case 4:
-                monthStr = 'May'
-                break
-            case 5:
-                monthStr = 'Jun';
-                break
-            case 6:
-                monthStr = 'Jul';
-                break
-            case 7:
-                monthStr = 'Aug';
-                break
-            case 8:
-                monthStr = 'Sep';
-                break
-            case 9:
-                monthStr = 'Oct';
-                break
-            case 10:
-                monthStr = 'Nov';
-                break
-            case 11:
-                monthStr = 'Dec';
-                break
-            default:
-                console.log('Invalid Month');
-        }
-          
-        let defaultDateString = `${monthStr} ${createdAt.getDay()}, ${createdAt.getFullYear()} at ${hour}:${createdAt.getMinutes()} ${AMPM}}`
+        var defaultDateString = `${createdAt.toLocaleDateString()} at ${createdAt.toLocaleTimeString()}`
 
         return defaultDateString
     }
