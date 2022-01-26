fd3d3471fbb46f1cc18f9aeca5bd31696057e27f
Evan Greer <evan.greer@iterable.com>
2022-01-26 15:52:28 -0700

    minor edits

--- ts/IterableInAppMessage.ts
+++ ts/IterableInAppMessage.ts
@@ -111,12 +111,10 @@ class IterableInAppMessage {
     const campaignId = dict["campaignId"] as number
     const trigger = IterableInAppTrigger.fromDict(dict["trigger"])
     let createdAt = dict["createdAt"]
-    console.log("from Dict 1 - ", createdAt)
     if (createdAt) {
       var dateObject = new Date(0)
       createdAt = dateObject.setUTCMilliseconds(createdAt)
     }
-    console.log("from Dict 2 - ", createdAt)
     let expiresAt = dict["expiresAt"]
     if (expiresAt) {
       var dateObject = new Date(0)
