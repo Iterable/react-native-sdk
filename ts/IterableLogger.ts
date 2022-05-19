de042e05e7bb00a196d0ec70ad7cee5663381269
Jay Kim <jay@nullavenue.com>
2022-05-19 14:16:19 -0700

    add comment on default true

--- ts/IterableLogger.ts
+++ ts/IterableLogger.ts
@@ -2,6 +2,8 @@ import { Iterable } from "./Iterable"
 
 class IterableLogger {
     log(message: String) {
+        // default to `true` in the case of unit testing where `Iterable` is not initialized
+        // which is most likely in a debug environment anyways
         var loggingEnabled = Iterable.savedConfig?.logReactNativeSdkCalls ?? true
 
         if (loggingEnabled) {
