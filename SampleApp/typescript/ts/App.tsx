e7a82c2fa5c92915810f266143687080de8e06a5
Evan Greer <evan.greer@iterable.com>
2022-11-11 15:54:32 -0700

    sets up eslint config

--- SampleApp/typescript/ts/App.tsx
+++ SampleApp/typescript/ts/App.tsx
@@ -6,14 +6,25 @@ import Icon from 'react-native-vector-icons/Ionicons'
 import HomeTab from './HomeTab'
 import SettingsTab from './SettingsTab'
 import { Coffee, coffees } from './Data'
+
+// Step 1: 
+// Import the Iterable and IterableConfig class. 
+// Refer to "Installing Iterable's React Native SDK" of the developer guide.
+// (https://support.iterable.com/hc/en-us/articles/360045714132-Installing-Iterable-s-React-Native-SDK-#step-5-import-iterable-sdk-classes-wherever-needed)
+
 import {
   Iterable,
   IterableConfig,
   IterableActionContext,
 } from '@iterable/react-native-sdk';
 
-// ITERABLE:
-// Make sure you have a file called Config.js and your apiKey is in there.
+
+// Step 2: 
+// Create a Config.js and add your apiKey as "iterableAPIKey".
+// Refer to "Creating API Keys" under "API Keys" of the developer guide 
+// to create an API key for your Iterable project.
+// (https://support.iterable.com/hc/en-us/articles/360043464871#creating-api-keys)
+
 import { iterableAPIKey } from './Config'
 
 interface Props { }
@@ -22,10 +33,26 @@ export default class App extends React.Component {
     super(props)
     this.homeTabRef = React.createRef()
 
-    // ITERABLE:
+    // Step 3: Initialize the React Native SDK here.
+    // Create an IterableConfig object with various properties set.
+    // The config object is used to customize various features of the SDK such as the URL handler and custom action handler.
+    // If a config object is not defined, the React Native SDK defaults to the default configuration.
+
+    // The IterableConfig.js file is linked to the documentation.
+    // Refer to Step 6 of "Installing Iterable's React Native SDK" of the developer guide.
+    // (https://support.iterable.com/hc/en-us/articles/360045714132-Installing-Iterable-s-React-Native-SDK-#step-6-initialize-iterable-s-react-native-sdk)
+
+    // Below is a sample implementation of the config object where we set the urlHAndler and inAppDisplayInterval
+        
     const config = new IterableConfig()
-    config.inAppDisplayInterval = 1.0 // Min gap between in-apps. No need to set this in production.
+
+    // inAppDisplayInterval sets the number of seconds to wait between displaying multiple in-app messages in sequence
+    config.inAppDisplayInterval = 1.0
+
+    // urlHandler is set up here to handle deep link URLs and in-app message buttons and link URLs
     config.urlHandler = this.urlHandler
+
+    // Initialize by calling the Iterable.initialize method passing in your API key and the optional config object.
     Iterable.initialize(iterableAPIKey, config)
   }
 
@@ -68,7 +95,7 @@ export default class App extends React.Component {
     }
   }
 
-  // ITERABLE:
+  // urlHandler is defined here
   private urlHandler = (url: String, context: IterableActionContext): boolean => {
     console.log(`urlHandler, url: ${url}`)
     let match = url.match(/coffee\/([^\/]+)/i)
