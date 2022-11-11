bdeadfbe88437b11e6dcb699afd7791389c88985
Evan Greer <evan.greer@iterable.com>
2022-11-11 11:17:11 -0700

    adds detailed comments for the sample apps

--- SampleApp/javascript/js/App.js
+++ SampleApp/javascript/js/App.js
@@ -5,38 +5,46 @@ import Icon from 'react-native-vector-icons/Ionicons';
 import HomeTab from './HomeTab';
 import SettingsTab from './SettingsTab';
 import { coffees } from './Data';
+
+// Step 1: 
+// Import the Iterable and IterableConfig class. 
+// Refer to "Installing Iterable's React Native SDK" of the developer guide.
+// (https://support.iterable.com/hc/en-us/articles/360045714132-Installing-Iterable-s-React-Native-SDK-#step-5-import-iterable-sdk-classes-wherever-needed)
+
 import { Iterable, IterableConfig, } from '@iterable/react-native-sdk';
-// ITERABLE:
-// Make sure you have a file called Config.js and your apiKey is in there.
+
+// Step 2: 
+// Create a Config.js and add your apiKey as "iterableAPIKey".
+// Refer to "Creating API Keys" under "API Keys" of the developer guide 
+// to create an API key for your Iterable project.
+// (https://support.iterable.com/hc/en-us/articles/360043464871#creating-api-keys)
+
 import { iterableAPIKey } from './Config';
+
 export default class App extends React.Component {
     constructor(props) {
         super(props);
-        // ITERABLE:
-        this.urlHandler = (url, context) => {
-            console.log(`urlHandler, url: ${url}`);
-            let match = url.match(/coffee\/([^\/]+)/i);
-            if (match && match.length > 1) {
-                const id = match[1];
-                const foundCoffee = coffees.find(coffee => coffee.id == id);
-                if (foundCoffee) {
-                    this.navigate(foundCoffee);
-                }
-                else {
-                    console.log(`could not find coffee with id: ${id}`);
-                }
-                return true;
-            }
-            else {
-                console.log("opening external url");
-                return false;
-            }
-        };
         this.homeTabRef = React.createRef();
-        // ITERABLE:
+
+        // Step 3: Initialize the React Native SDK here.
+        // Create an IterableConfig object with various properties set.
+        // The config object is used to customize various features of the SDK such as the URL handler and custom action handler.
+        // If a config object is not defined, the React Native SDK defaults to the default configuration.
+
+        // The IterableConfig.js file is linked to the documentation.
+        // Refer to Step 6 of "Installing Iterable's React Native SDK" of the developer guide.
+        // (https://support.iterable.com/hc/en-us/articles/360045714132-Installing-Iterable-s-React-Native-SDK-#step-6-initialize-iterable-s-react-native-sdk)
+        
+        // Below is a sample implementation of the config object where we set the urlHAndler and inAppDisplayInterval
         const config = new IterableConfig();
-        config.inAppDisplayInterval = 1.0; // Min gap between in-apps. No need to set this in production.
+
+        // urlHandler is set up here to handle deep link URLs and in-app message buttons and link URLs
         config.urlHandler = this.urlHandler;
+
+        // inAppDisplayInterval sets the number of seconds to wait between displaying multiple in-app messages in sequence
+        config.inAppDisplayInterval = 1.0;
+
+        // Initialize by calling the Iterable.initialize method passing in your API key and the optional config object. 
         Iterable.initialize(iterableAPIKey, config);
     }
     render() {
@@ -64,4 +72,25 @@ export default class App extends React.Component {
             this.homeTabRef.current.navigate(coffee);
         }
     }
+
+    // urlHandler is defined here
+    urlHandler = (url, context) => {
+        console.log(`urlHandler, url: ${url}`);
+        let match = url.match(/coffee\/([^\/]+)/i);
+        if (match && match.length > 1) {
+            const id = match[1];
+            const foundCoffee = coffees.find(coffee => coffee.id == id);
+            if (foundCoffee) {
+                this.navigate(foundCoffee);
+            }
+            else {
+                console.log(`could not find coffee with id: ${id}`);
+            }
+            return true;
+        }
+        else {
+            console.log("opening external url");
+            return false;
+        }
+    };
 }
