1495e74a4d772bdb50ef34b6f3325cc8387f39d6
Evan Greer <evan.greer@iterable.com>
2022-11-13 10:54:15 -0700

    adds es lint script to package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,8 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
+    project: ['./tsconfig.json']
   },
   plugins: [
     'react',
