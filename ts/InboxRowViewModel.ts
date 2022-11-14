9113851df597adca73b293c26c3e73ee57a95464
Evan Greer <evan.greer@iterable.com>
2022-11-14 08:12:38 -0700

    adds linting on saving

--- ts/InboxRowViewModel.ts
+++ ts/InboxRowViewModel.ts
@@ -2,13 +2,13 @@
 
 import IterableInAppMessage from './IterableInAppMessage'
 
-type InboxRowViewModel = {
-    title: string
-    subtitle?: string
-    imageUrl?: string
-    createdAt?: Date
-    read: boolean
-    inAppMessage: IterableInAppMessage
+interface InboxRowViewModel {
+  title: string
+  subtitle?: string
+  imageUrl?: string
+  createdAt?: Date
+  read: boolean
+  inAppMessage: IterableInAppMessage
 }
 
 export default InboxRowViewModel
