bb8d4d0b66e97f32e3a1bbf5dfc0ad9c4aac041d
Evan Greer <evan.greer@iterable.com>
2022-12-11 08:55:32 -1000

    updates imports

--- ts/IterableConfig.ts
+++ ts/IterableConfig.ts
@@ -4,7 +4,7 @@ import {
   IterableAction, 
   IterableActionContext, 
   IterableLogLevel,
-} from './IterableConfig'
+} from './IterableAction'
 
 import { IterableInAppShowResponse } from './IterableInAppClasses'
 
