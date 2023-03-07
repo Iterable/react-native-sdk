abe392cf087ab8e2fb1e287d6e3968b5cd89c64b
Jay Kim <jay@nullavenue.com>
2023-03-07 14:26:30 -0800

    readjust data type and android binding

--- android/src/main/java/com/iterable/iterableapi/RNIterableInternal.java
+++ android/src/main/java/com/iterable/iterableapi/RNIterableInternal.java
@@ -2,16 +2,7 @@ package com.iterable.iterableapi;
 
 import androidx.annotation.Nullable;
 
-import com.facebook.react.bridge.WritableArray;
-import com.facebook.react.bridge.WritableMap;
-import com.facebook.react.bridge.WritableNativeArray;
-import com.facebook.react.bridge.WritableNativeMap;
-
-import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.util.Iterator;
 
 public class RNIterableInternal {
 
@@ -26,17 +17,7 @@ public class RNIterableInternal {
     }
 
     public static JSONObject getInAppMessageJson(IterableInAppMessage message) {
-        JSONObject messageJson = message.toJSONObject();
-
-        if (message.getCustomPayload() != null) {
-            try {
-                messageJson.put(IterableConstants.ITERABLE_IN_APP_CUSTOM_PAYLOAD, convertJsonToMap(message.getCustomPayload()));
-            } catch (JSONException e) {
-                // something crazy happened if we're here so we'll just skip
-            }
-        }
-
-        return messageJson;
+        return message.toJSONObject();
     }
 
     public static IterableInAppMessage getMessageById(String messageId) {
@@ -49,58 +30,5 @@ public class RNIterableInternal {
 
     public static void setAttributionInfo(IterableAttributionInfo attributionInfo) {
         IterableApi.getInstance().setAttributionInfo(attributionInfo);
-    }
-
-    // obtained from https://gist.github.com/viperwarp/2beb6bbefcc268dee7ad
-    // copied over from Serialization.java for expediency, this can definitely be reorganized better
-
-    static WritableMap convertJsonToMap(JSONObject jsonObject) throws JSONException {
-        WritableMap map = new WritableNativeMap();
-
-        Iterator<String> iterator = jsonObject.keys();
-        while (iterator.hasNext()) {
-            String key = iterator.next();
-            Object value = jsonObject.get(key);
-            if (value instanceof JSONObject) {
-                map.putMap(key, convertJsonToMap((JSONObject) value));
-            } else if (value instanceof JSONArray) {
-                map.putArray(key, convertJsonToArray((JSONArray) value));
-            } else if (value instanceof Boolean) {
-                map.putBoolean(key, (Boolean) value);
-            } else if (value instanceof Integer) {
-                map.putInt(key, (Integer) value);
-            } else if (value instanceof Double) {
-                map.putDouble(key, (Double) value);
-            } else if (value instanceof String) {
-                map.putString(key, (String) value);
-            } else {
-                map.putString(key, value.toString());
-            }
-        }
-        return map;
-    }
-
-    static WritableArray convertJsonToArray(JSONArray jsonArray) throws JSONException {
-        WritableArray array = new WritableNativeArray();
-
-        for (int i = 0; i < jsonArray.length(); i++) {
-            Object value = jsonArray.get(i);
-            if (value instanceof JSONObject) {
-                array.pushMap(convertJsonToMap((JSONObject) value));
-            } else if (value instanceof JSONArray) {
-                array.pushArray(convertJsonToArray((JSONArray) value));
-            } else if (value instanceof Boolean) {
-                array.pushBoolean((Boolean) value);
-            } else if (value instanceof Integer) {
-                array.pushInt((Integer) value);
-            } else if (value instanceof Double) {
-                array.pushDouble((Double) value);
-            } else if (value instanceof String) {
-                array.pushString((String) value);
-            } else {
-                array.pushString(value.toString());
-            }
-        }
-        return array;
     }
 }
