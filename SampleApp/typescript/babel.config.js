e4eb49140a99a66f3d56089a8d2d8c7cc34f13ac
Evan Greer <evan.greer@evan.greer>
2024-06-17 16:03:00 -0600

    Revert "adds deployment target script to podfile and typscript sample app update"
    This reverts commit 2d51aa65d5362e70a8810fc374f197729b0fca8d.

--- SampleApp/typescript/babel.config.js
+++ SampleApp/typescript/babel.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-  presets: [['module:metro-react-native-babel-preset', {
-    unstable_disableES6Transforms: true
-  }]],
+  presets: ['module:metro-react-native-babel-preset'],
 };
