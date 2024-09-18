092ca93cbb7f22f6930ea9d1b505056e08dce376
Loren Posen <loren.posen@iterable.com>
2024-09-17 19:41:16 -0700

    Removed deprecated `removeStateListener` function

--- ts/useAppStateListener.ts
+++ ts/useAppStateListener.ts
@@ -1,29 +1,23 @@
-import { useEffect, useRef, useState } from 'react'
-import { AppState } from 'react-native'
+import { useEffect, useRef, useState } from 'react';
+import { AppState, type AppStateStatus } from 'react-native';
 
-function useAppStateListener() {
-    const appStateEventName = "change"
-    const appState = useRef(AppState.currentState)
-    const [appStateVisibility, setAppStateVisibility] = useState(appState.current)
+function useAppStateListener(): AppStateStatus {
+  const appStateEventName = 'change';
+  const appState = useRef(AppState.currentState);
+  const [appStateVisibility, setAppStateVisibility] = useState(appState.current);
 
-    useEffect(() => {
-        AppState.addEventListener(
-            appStateEventName,
-            (nextAppState) => {
-                appState.current = nextAppState;
-                setAppStateVisibility(appState.current)
-            }
-        )
+  useEffect(() => {
+    const listener = AppState.addEventListener(appStateEventName, (nextAppState) => {
+      appState.current = nextAppState;
+      setAppStateVisibility(appState.current);
+    });
 
-        return () => {
-            AppState.removeEventListener(
-                appStateEventName,
-                () => { }
-            )
-        }
-    }, [])
+    return () => {
+      listener?.remove();
+    };
+  }, []);
 
-    return appStateVisibility
+  return appStateVisibility;
 }
 
-export default useAppStateListener
+export default useAppStateListener;
