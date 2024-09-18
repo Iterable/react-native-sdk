b436115a5fb49797b4f78d32aa9e264a756dc509
Loren Posen <loren.posen@iterable.com>
2024-09-17 20:24:43 -0700

    Updated configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,17 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
     es2021: true,
     jest: true,
     'react-native/react-native': true,
   },
-  extends: ['plugin:react/recommended', 'standard-with-typescript', 'airbnb/hooks'],
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'airbnb/hooks',
+    '@react-native',
+  ],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
