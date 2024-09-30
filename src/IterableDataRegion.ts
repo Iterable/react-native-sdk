6df54f5a70c9cd04364c521403960f371d825cd3
Loren Posen <loren.posen@iterable.com>
2024-09-30 15:07:26 -0700

    Refactor IterableDataRegion and export as default

--- src/IterableDataRegion.ts
+++ src/IterableDataRegion.ts
@@ -1,8 +1,7 @@
-enum IterableDataRegion {
-    US = 0,
-    EU = 1
-  }
-
-export {
-    IterableDataRegion
+// TODO: Add a description
+export enum IterableDataRegion {
+  US = 0,
+  EU = 1,
 }
+
+export default IterableDataRegion;
