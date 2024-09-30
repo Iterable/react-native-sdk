adc6b929d8d64b8e6a788166f8fd9eba080b98b4
Loren Posen <loren.posen@iterable.com>
2024-09-30 15:55:12 -0700

    fixed index file

--- src/index.tsx
+++ src/index.tsx
@@ -2,72 +2,33 @@
  * React Native module for Iterable.
  * @module react-native-iterable-sdk
  */
-
-import { Iterable, IterableCommerceItem } from './Iterable';
-import IterableInAppManager from './IterableInAppManager';
-
-import {
+export type { InboxImpressionRowInfo } from './InboxImpressionRowInfo';
+export type { InboxRowViewModel } from './InboxRowViewModel';
+export { Iterable, IterableCommerceItem } from './Iterable';
+export {
   IterableAction,
   IterableActionContext,
   IterableLogLevel,
 } from './IterableAction';
-
-import {
-  IterableInAppShowResponse,
-  type IterableInAppContent,
-  IterableInAppTriggerType,
-  IterableInAppTrigger,
-  IterableInAppContentType,
-  IterableEdgeInsets,
-  IterableHtmlInAppContent,
-  IterableInboxMetadata,
-  IterableInAppLocation,
-  IterableInAppCloseSource,
-  IterableInAppDeleteSource,
-} from './IterableInAppClasses';
-
-import type { InboxRowViewModel } from './InboxRowViewModel';
-import IterableInboxCustomizations from './IterableInboxCustomizations';
-import IterableInboxEmptyState from './IterableInboxEmptyState';
-import IterableInboxMessageCell from './IterableInboxMessageCell';
-
-import IterableInAppMessage from './IterableInAppMessage';
-
-import useAppStateListener from './useAppStateListener';
-import useDeviceOrientation from './useDeviceOrientation';
-import InboxImpressionRowInfo from './InboxImpressionRowInfo';
-
-import IterableConfig from './IterableConfig';
-import { IterableDataRegion } from './IterableDataRegion';
-
+export { IterableConfig } from './IterableConfig';
+export { IterableDataRegion } from './IterableDataRegion';
 export {
-  Iterable,
-  IterableCommerceItem,
-  IterableConfig,
-  IterableInAppManager,
-  IterableAction,
-  IterableActionContext,
-  IterableLogLevel,
-  IterableInAppShowResponse,
-  IterableInAppTriggerType,
-  IterableInAppTrigger,
-  IterableInAppContentType,
   IterableEdgeInsets,
   IterableHtmlInAppContent,
-  IterableInboxMetadata,
-  IterableInAppLocation,
-  IterableInAppMessage,
   IterableInAppCloseSource,
+  IterableInAppContentType,
   IterableInAppDeleteSource,
-  IterableInboxEmptyState,
-  IterableInboxMessageCell,
-  useAppStateListener,
-  useDeviceOrientation,
-  IterableDataRegion,
-};
-export type {
-  IterableInAppContent,
-  IterableInboxCustomizations,
-  InboxRowViewModel,
-  InboxImpressionRowInfo,
-};
+  IterableInAppLocation,
+  IterableInAppShowResponse,
+  IterableInAppTrigger,
+  IterableInAppTriggerType,
+  IterableInboxMetadata,
+  type IterableInAppContent,
+} from './IterableInAppClasses';
+export { IterableInAppManager } from './IterableInAppManager';
+export { IterableInAppMessage } from './IterableInAppMessage';
+export type { IterableInboxCustomizations } from './IterableInboxCustomizations';
+export { IterableInboxEmptyState } from './IterableInboxEmptyState';
+export { IterableInboxMessageCell } from './IterableInboxMessageCell';
+export { useAppStateListener } from './useAppStateListener';
+export { useDeviceOrientation } from './useDeviceOrientation';
