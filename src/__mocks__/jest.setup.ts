76fe02096206bcad168ec9ca55b1cc2b04f43ce7
Loren Posen <loren.posen@iterable.com>
2024-10-08 17:55:04 -0700

    Tests working

--- src/__mocks__/jest.setup.ts
+++ src/__mocks__/jest.setup.ts
@@ -5,6 +5,13 @@ import { MockLinking } from './MockLinking';
 
 jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter.js');
 
+jest.mock('react-native-webview', () => {
+  const { View } = require('react-native');
+  return {
+    WebView: View,
+  };
+});
+
 jest.doMock('react-native', () => {
   // Extend ReactNative
   return Object.setPrototypeOf(
