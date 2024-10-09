c10f25ed642bdba2108e016309fbcdf4d93eb7fa
Loren Posen <loren.posen@iterable.com>
2024-10-08 17:55:23 -0700

    Removed old jest config file

--- jest_OLD.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: 'react-native',
-  setupFiles: ['<rootDir>/ts/__mocks__/jest.setup.ts'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testMatch: ['<rootDir>/ts/__tests__/**/*.(test|spec).[jt]s?(x)']
-}
