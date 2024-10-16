48db65134a82d863128e2398ca935ea64fa1a3db
Loren Posen <loren.posen@iterable.com>
2024-10-16 13:03:01 -0700

    Replace hardcoded API key and user ID in e2e test with environment variables

--- example/e2e/starter.test.js
+++ example/e2e/starter.test.js
@@ -1,5 +1,5 @@
-const API_KEY = '42404ed640b44645ac8133499b515e23';
-const USER = 'loren.posen@iterable.com';
+import { device, element, by, expect, waitFor } from 'detox';
+import { ITBL_API_KEY, ITBL_ID } from '@env';
 
 describe('Example', () => {
   beforeAll(async () => {
@@ -10,8 +10,8 @@ describe('Example', () => {
 
   it('should be able to login', async () => {
     await expect(element(by.text('Login'))).toBeVisible();
-    await element(by.id('api-key')).replaceText(API_KEY);
-    await element(by.id('user-id')).replaceText(USER);
+    await element(by.id('api-key')).replaceText(ITBL_API_KEY);
+    await element(by.id('user-id')).replaceText(ITBL_ID);
     await element(by.text('Login')).tap();
     await waitFor(element(by.id('inbox-header'))).toBeVisible();
   });
