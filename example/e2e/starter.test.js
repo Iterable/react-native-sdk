c9421f7437b90dfae08bdb6d51315cd61acb4cbf
Loren Posen <loren.posen@iterable.com>
2024-10-15 20:10:00 -0700

    Enhance login functionality and update test cases; add Detox types and ESLint plugin

--- example/e2e/starter.test.js
+++ example/e2e/starter.test.js
@@ -3,21 +3,18 @@ describe('Example', () => {
     await device.launchApp();
   });
 
-  beforeEach(async () => {
-    await device.reloadReactNative();
-  });
+  beforeEach(async () => {});
 
-  it('should have welcome screen', async () => {
-    await expect(element(by.id('welcome'))).toBeVisible();
-  });
-
-  it('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.text('Hello!!!'))).toBeVisible();
-  });
-
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.text('World!!!'))).toBeVisible();
+  it('should be able to login', async () => {
+    await expect(element(by.text('Login'))).toBeVisible();
+    // await element(by.id('api-key')).tap();
+    await element(by.id('api-key')).replaceText(
+      '42404ed640b44645ac8133499b515e23'
+    );
+    await element(by.id('user-id')).replaceText('loren.posen@iterable.com');
+    await element(by.text('Login')).tap();
+    await waitFor(element(by.id('inbox')))
+      .toBeVisible()
+      .withTimeout(5000);
   });
 });
