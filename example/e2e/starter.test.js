dfbb36179e418810220f54d6010ae57cd0824fa5
Loren Posen <loren.posen@iterable.com>
2024-10-16 03:01:52 -0700

    Update test and component to improve login flow and visibility checks

--- example/e2e/starter.test.js
+++ example/e2e/starter.test.js
@@ -8,13 +8,9 @@ describe('Example', () => {
   it('should be able to login', async () => {
     await expect(element(by.text('Login'))).toBeVisible();
     // await element(by.id('api-key')).tap();
-    await element(by.id('api-key')).replaceText(
-      '42404ed640b44645ac8133499b515e23'
-    );
+    await element(by.id('api-key')).replaceText('42404ed640b44645ac8133499b515e23');
     await element(by.id('user-id')).replaceText('loren.posen@iterable.com');
     await element(by.text('Login')).tap();
-    await waitFor(element(by.id('inbox')))
-      .toBeVisible()
-      .withTimeout(5000);
+    await waitFor(element(by.id('inbox-header'))).toBeVisible();
   });
 });
