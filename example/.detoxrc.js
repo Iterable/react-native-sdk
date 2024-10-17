193117379192adaf2343510f1f52a393a83b5736
Loren Posen <loren.posen@iterable.com>
2024-10-17 10:49:06 -0700

    Enhance Detox configuration: update logging levels, add new domain for network security, and refine CI test commands

--- example/.detoxrc.js
+++ example/.detoxrc.js
@@ -1,84 +1,109 @@
 /** @type {Detox.DetoxConfig} */
 module.exports = {
+  logger: {
+    level: process.env.CI ? 'debug' : undefined,
+  },
   testRunner: {
     args: {
-      '$0': 'jest',
-      config: 'e2e/jest.config.js'
+      $0: 'jest',
+      config: 'e2e/jest.config.js',
+      maxWorkers: process.env.CI ? 2 : undefined,
     },
     jest: {
-      setupTimeout: 120000
-    }
+      setupTimeout: 120000,
+    },
+  },
+  artifacts: {
+    plugins: {
+      log: process.env.CI ? 'failing' : undefined,
+      screenshot: process.env.CI ? 'failing' : undefined,
+    },
   },
   apps: {
     'ios.debug': {
       type: 'ios.app',
       binaryPath: 'ios/build/Build/Products/Debug-iphonesimulator/ReactNativeSdkExample.app',
-      build: 'xcodebuild -workspace ios/ReactNativeSdkExample.xcworkspace -scheme ReactNativeSdkExample -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build'
+      build:
+        'xcodebuild -workspace ios/ReactNativeSdkExample.xcworkspace -scheme ReactNativeSdkExample -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build',
     },
     'ios.release': {
       type: 'ios.app',
       binaryPath: 'ios/build/Build/Products/Release-iphonesimulator/ReactNativeSdkExample.app',
-      build: 'xcodebuild -workspace ios/ReactNativeSdkExample.xcworkspace -scheme ReactNativeSdkExample -configuration Release -sdk iphonesimulator -derivedDataPath ios/build'
+      build:
+        'xcodebuild -workspace ios/ReactNativeSdkExample.xcworkspace -scheme ReactNativeSdkExample -configuration Release -sdk iphonesimulator -derivedDataPath ios/build',
     },
     'android.debug': {
       type: 'android.apk',
       binaryPath: 'android/app/build/outputs/apk/debug/app-debug.apk',
       build: 'cd android && ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug',
-      reversePorts: [
-        8081
-      ]
+      reversePorts: [8081],
     },
     'android.release': {
       type: 'android.apk',
       binaryPath: 'android/app/build/outputs/apk/release/app-release.apk',
-      build: 'cd android && ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release'
-    }
+      build: 'cd android && ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release',
+    },
   },
   devices: {
     simulator: {
       type: 'ios.simulator',
+      headless: Boolean(process.env.CI),
       device: {
-        type: 'iPhone SE (3rd generation)'
+        type: 'iPhone SE (3rd generation)',
         // type: 'iPhone 11'
-      }
+      },
     },
     attached: {
       type: 'android.attached',
       device: {
-        adbName: '.*'
-      }
+        adbName: '.*',
+      },
     },
     emulator: {
       type: 'android.emulator',
+      headless: Boolean(process.env.CI),
+      gpuMode: process.env.CI ? 'off' : undefined,
       device: {
-        avdName: 'Pixel_3a_API_34'
-      }
-    }
+        avdName: 'Pixel_3a_API_34',
+      },
+    },
   },
   configurations: {
     'ios.sim.debug': {
       device: 'simulator',
-      app: 'ios.debug'
+      app: 'ios.debug',
     },
     'ios.sim.release': {
       device: 'simulator',
-      app: 'ios.release'
+      app: 'ios.release',
+    },
+    'ios.manual': {
+      type: 'ios.manual',
+      behavior: {
+        launchApp: 'manual',
+      },
+      artifacts: false,
+      session: {
+        autoStart: true,
+        server: 'ws://localhost:8099',
+        sessionId: 'com.iterable.reactnativesdk.example',
+      },
     },
     'android.att.debug': {
       device: 'attached',
-      app: 'android.debug'
+      app: 'android.debug',
     },
     'android.att.release': {
       device: 'attached',
-      app: 'android.release'
+      app: 'android.release',
     },
     'android.emu.debug': {
       device: 'emulator',
-      app: 'android.debug'
+      app: 'android.debug',
     },
     'android.emu.release': {
       device: 'emulator',
-      app: 'android.release'
-    }
-  }
+      app: 'android.release',
+    },
+  },
 };
