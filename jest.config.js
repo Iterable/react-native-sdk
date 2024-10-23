2146588949e798f5eb82703eabb35699623da615
Loren Posen <loren.posen@iterable.com>
2024-10-23 16:26:31 -0700

    Update testing setup and coverage configuration: add coverage script, mock WebView, and restructure test files

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,28 @@
 module.exports = {
   preset: 'react-native',
-  setupFiles: ['<rootDir>/ts/__mocks__/jest.setup.ts'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testMatch: ['<rootDir>/ts/__tests__/**/*.(test|spec).[jt]s?(x)'],
+  setupFiles: ['<rootDir>/src/__mocks__/jest.setup.ts'],
+  setupFilesAfterEnv: [
+    '<rootDir>/node_modules/@testing-library/jest-native/extend-expect',
+  ],
+  testMatch: ['<rootDir>/src/__tests__/**/*.(test|spec).[jt]s?(x)'],
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native(-.*)?|@react-native(-community)?)/)',
+  ],
+  collectCoverageFrom: [
+    'src/**/*.{cjs,js,jsx,mjs,ts,tsx}',
+    '!src/**/*.test.{cjs,js,jsx,mjs,mdx,ts,tsx}',
+    '!src/(__tests__|__mocks__)/*',
+  ],
+  modulePathIgnorePatterns: [
+    '<rootDir>/example/node_modules',
+    '<rootDir>/lib/',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 10,
+      functions: 30,
+      lines: 30,
+      statements: 30,
+    },
+  },
 };
