1d73a8dd8b9c140ce7fea0b1e8be750cd18c2cd2
Loren Posen <loren.posen@iterable.com>
2024-11-07 13:08:56 -0800

    Enhance ESLint configuration for TypeScript support: add TypeScript parser and plugin, and update rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,14 @@
 module.exports = {
   root: true,
-  extends: ['@react-native', 'prettier'],
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: ['@react-native', 'prettier', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
   rules: {
     'react/react-in-jsx-scope': 'off',
     'prettier/prettier': [
