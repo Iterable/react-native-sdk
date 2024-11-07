2ce56ae8af0e32c8f2423be2045f714203ffcf65
Loren Posen <loren.posen@iterable.com>
2024-11-07 12:57:30 -0800

    Add ESLint configuration file and remove inline config from package.json

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  extends: ['@react-native', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        quoteProps: 'consistent',
+        singleQuote: true,
+        tabWidth: 2,
+        trailingComma: 'es5',
+        useTabs: false,
+      },
+    ],
+  },
+};
