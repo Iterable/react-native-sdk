0bdaf82ef9722f4e1c2dae5a9955a5d2367e2dc2
Loren Posen <loren.posen@iterable.com>
2024-12-13 16:53:54 -0800

    refactor: removed unused ios files adding in scaffolding

--- ios/ReactNativeSdk.swift
+++
@@ -1,8 +0,0 @@
-@objc(ReactNativeSdk)
-class ReactNativeSdk: NSObject {
-
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
-  }
-}
