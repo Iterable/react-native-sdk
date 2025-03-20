59a6885c8487231e2e2eeb3d5844bf730f5641b8
Loren Posen <loren.posen@iterable.com>
2025-03-20 16:42:22 -0700

    testing again

--- app.plugin.js
+++ app.plugin.js
@@ -1,2 +1,14 @@
-// module.exports = require('./src/plugin/withIterableSDK');
-module.exports = require('./lib/commonjs/plugin/withIterableSDK').default;
+// Using CommonJS syntax instead of ES modules
+// const { withPlugins } = require('@expo/config-plugins');
+
+// const withIterableSDK = (config) => {
+//   return withPlugins(config, [
+//     // Your plugin modifications will go here
+//   ]);
+// };
+
+// module.exports = withIterableSDK;
+
+// /Users/Loren.Posen/mobile/RN/react-native-sdk/lib/commonjs/plugin/withIterableSDK.js
+
+module.exports = require('./lib/commonjs/plugin/withIterableSDK');
--- src/plugin/withIterableSDK.ts
+++ src/plugin/withIterableSDK.ts
@@ -1,22 +1,37 @@
+// import { type ConfigPlugin } from '@expo/config-plugins';
+
+// const withIterableSDK: ConfigPlugin = (config) => {
+//   console.log('withIterableSDK', config);
+//   // iOS configuration
+//   // config = withInfoPlist(config, (config) => {
+//   //   // Add any required iOS permissions or configurations
+//   //   return config;
+//   // });
+
+//   // // Android configuration
+//   // config = withAndroidManifest(config, (config) => {
+//   //   // Add any required Android permissions or configurations
+//   //   return config;
+//   // });
+
+//   return config;
+// };
+
+// export default withIterableSDK;
+
 import { type ConfigPlugin, createRunOncePlugin } from '@expo/config-plugins';
 
-const pkg = require('../package.json');
+const withIterableSDK: ConfigPlugin = (config, _props) => {
+  console.log(` _props:`, _props);
+  console.log(` config:`, config);
+  // const props = _props || { androidApiKey: '', iosApiKey: '', baseUrl: '' };
 
-const withIterableSDK: ConfigPlugin = (config) => {
-  console.log('withIterableSDK', config);
-  // iOS configuration
-  // config = withInfoPlist(config, (config) => {
-  //   // Add any required iOS permissions or configurations
-  //   return config;
-  // });
-
-  // // Android configuration
-  // config = withAndroidManifest(config, (config) => {
-  //   // Add any required Android permissions or configurations
-  //   return config;
-  // });
+  // config = withAndroidBrazeSdk(config, props);
+  // config = withIOSBrazeSdk(config, props);
 
   return config;
 };
+
+const pkg = require('../package.json');
 
 export default createRunOncePlugin(withIterableSDK, pkg.name, pkg.version);
