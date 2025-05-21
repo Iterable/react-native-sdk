d812d07e8dd642233642ba49d877192ac11efbc6
Evan Greer <evan.greer@evan.greer>
2025-05-21 15:41:34 -0600

    feat: adds getEmbeddedPlacements to native layers

--- src/embedded/classes/IterableEmbeddedManager.ts
+++ src/embedded/classes/IterableEmbeddedManager.ts
@@ -5,7 +5,17 @@ import { IterableEmbeddedPlacement } from './IterableEmbeddedPlacement';
 
 const RNIterableAPI = NativeModules.RNIterableAPI;
 
+/**
+ * Manages embedded messages for the current user.
+ *
+ * This class provides methods to interact with embedded messages, including retrieving placements.
+ */
 export class IterableEmbeddedManager {
+  /**
+   * Retrieve the current user's list of embedded placements.
+   *
+   * @returns A Promise that resolves to an array of embedded placements.
+   */
   getPlacements(): Promise<IterableEmbeddedPlacement[]> {
     Iterable?.logger?.log('EmbeddedManager.getPlacements');
 
