d83d2ec6cc63fd22f050b1c8ccd6bdace55180bf
Evan Greer <evan.greer@evan.greer>
2025-05-23 14:22:43 -0600

    feat: adds constructor and messages list to IterableEmbeddedPlacement

--- src/embedded/classes/IterableEmbeddedPlacement.ts
+++ src/embedded/classes/IterableEmbeddedPlacement.ts
@@ -1,3 +1,5 @@
+import { IterableEmbeddedMessage } from './IterableEmbeddedMessage';
+
 /**
  * Iterable embedded placement
  * Contains placement id and the associated embedded messages
@@ -5,7 +7,10 @@
 export class IterableEmbeddedPlacement {
   readonly placementId: number;
 
-  constructor(placementId: number) {
+  readonly messages: IterableEmbeddedMessage[];
+
+  constructor(placementId: number, messages: IterableEmbeddedMessage[]) {
     this.placementId = placementId;
+    this.messages = messages;
   }
 }
