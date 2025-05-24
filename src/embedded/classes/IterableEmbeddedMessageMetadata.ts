ac62585efb13122a5156209f9be1403cf9917ac7
Evan Greer <evan.greer@evan.greer>
2025-05-24 11:54:52 -0600

    feat: adds comments to IterableEmbeddedMessageMetadata

--- src/embedded/classes/IterableEmbeddedMessageMetadata.ts
+++ src/embedded/classes/IterableEmbeddedMessageMetadata.ts
@@ -1,9 +1,27 @@
+/**
+ * Metadata for an embedded message.
+ */
 export class IterableEmbeddedMessageMetadata {
+  /** The ID for the embedded message */
   readonly messageId: string;
+
+  /** The placement ID for the embedded message */
   readonly placementId: number;
+
+  /** The campaign ID for the embedded message */
   readonly campaignId?: number;
+
+  /** Whether the embedded message is a proof */
   readonly isProof: boolean;
 
+  /**
+   * Constructs an instance of IterableEmbeddedMessageMetadata.
+   *
+   * @param messageId - The ID for the embedded message.
+   * @param placementId - The placement ID for the embedded message.
+   * @param campaignId - The campaign ID for the embedded message.
+   * @param isProof - Whether the embedded message is a proof.
+   */
   constructor(
     messageId: string,
     placementId: number,
@@ -16,6 +34,14 @@ export class IterableEmbeddedMessageMetadata {
     this.isProof = isProof;
   }
 
+  /**
+   * Creates an instance of `IterableEmbeddedMessageMetadata` from a dictionary object.
+   *
+   * @param dict - The dictionary objectcontaining the metadata properties.
+   * This corresponds to the properties in {@link IterableEmbeddedMessageMetadata}
+   *
+   * @returns A new instance of `IterableEmbeddedMessageMetadata` with the provided properties.
+   */
   static fromDict(
     dict: Partial<EmbeddedMessageMetadataDict>
   ): IterableEmbeddedMessageMetadata {
@@ -31,6 +57,9 @@ export class IterableEmbeddedMessageMetadata {
   }
 }
 
+/**
+ * An interface defining the dictionary object containing the metadata properties for an embedded message.
+ */
 export interface EmbeddedMessageMetadataDict {
   messageId: string;
   placementId: number;
