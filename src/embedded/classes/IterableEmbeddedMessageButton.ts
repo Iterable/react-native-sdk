1ce1e39a3577c9ad6523e079a55101782ddd57e5
Evan Greer <evan.greer@evan.greer>
2025-05-27 13:23:18 -0600

    feat: adds IterableEmbeddedMessageButton class

--- src/embedded/classes/IterableEmbeddedMessageButton.ts
+++ src/embedded/classes/IterableEmbeddedMessageButton.ts
@@ -1,1 +1,32 @@
-export class IterableEmbeddedMessageButton {}
+import { IterableEmbeddedMessageElementsButtonAction } from './IterableEmbeddedMessageElementsButtonAction';
+
+export class IterableEmbeddedMessageButton {
+  readonly id: string;
+  readonly title?: string;
+  readonly action?: IterableEmbeddedMessageElementsButtonAction;
+
+  constructor(
+    id: string,
+    title?: string,
+    action?: IterableEmbeddedMessageElementsButtonAction
+  ) {
+    this.id = id;
+    this.title = title;
+    this.action = action;
+  }
+
+  static fromDict(
+    dict: Partial<EmbeddedMessageButtonDict>
+  ): IterableEmbeddedMessageButton {
+    if (!dict.id) {
+      throw new Error('id is required');
+    }
+    return new IterableEmbeddedMessageButton(dict.id, dict.title, dict.action);
+  }
+}
+
+export interface EmbeddedMessageButtonDict {
+  id: string;
+  title?: string;
+  action?: IterableEmbeddedMessageElementsButtonAction;
+}
