76ef3b91cfb4561149a81239104fdd4e6407c10f
Evan Greer <evan.greer@evan.greer>
2025-05-27 16:09:02 -0600

    feat: adds IterableEmbeddedMessageElements class

--- src/embedded/classes/IterableEmbeddedMessageElements.ts
+++ src/embedded/classes/IterableEmbeddedMessageElements.ts
@@ -3,21 +3,22 @@ import { IterableEmbeddedMessageButton } from './IterableEmbeddedMessageButton';
 import { IterableEmbeddedMessageText } from './IterableEmbeddedMessageText';
 
 export class IterableEmbeddedMessageElements {
-  readonly title: string;
-  readonly body: string;
+  readonly title?: string;
+  readonly body?: string;
   readonly mediaUrl?: string;
   readonly mediaUrlCaption?: string;
   readonly defaultAction?: IterableEmbeddedMessageDefaultAction;
   readonly buttons?: IterableEmbeddedMessageButton[];
   readonly text?: IterableEmbeddedMessageText[];
+
   constructor(
-    title: string,
-    body: string,
-    mediaUrl: string | undefined,
-    mediaUrlCaption: string | undefined,
-    defaultAction: IterableEmbeddedMessageDefaultAction | undefined,
-    buttons: IterableEmbeddedMessageButton[] | undefined,
-    text: IterableEmbeddedMessageText[] | undefined
+    title?: string,
+    body?: string,
+    mediaUrl?: string,
+    mediaUrlCaption?: string,
+    defaultAction?: IterableEmbeddedMessageDefaultAction,
+    buttons?: IterableEmbeddedMessageButton[],
+    text?: IterableEmbeddedMessageText[]
   ) {
     this.title = title;
     this.body = body;
@@ -27,4 +28,44 @@ export class IterableEmbeddedMessageElements {
     this.buttons = buttons;
     this.text = text;
   }
+
+  static fromDict(
+    dict: Partial<EmbeddedMessageElementsDict>
+  ): IterableEmbeddedMessageElements {
+    const title = dict.title;
+    const body = dict.body;
+    const mediaUrl = dict.mediaUrl;
+    const mediaUrlCaption = dict.mediaUrlCaption;
+    const defaultAction = dict.defaultAction
+      ? IterableEmbeddedMessageDefaultAction.fromDict(dict.defaultAction)
+      : undefined;
+
+    const buttons = dict.buttons?.map((button) =>
+      IterableEmbeddedMessageButton.fromDict(button)
+    );
+
+    const text = dict.text?.map((text) =>
+      IterableEmbeddedMessageText.fromDict(text)
+    );
+
+    return new IterableEmbeddedMessageElements(
+      title,
+      body,
+      mediaUrl,
+      mediaUrlCaption,
+      defaultAction,
+      buttons,
+      text
+    );
+  }
+}
+
+export interface EmbeddedMessageElementsDict {
+  title?: string;
+  body?: string;
+  mediaUrl?: string;
+  mediaUrlCaption?: string;
+  defaultAction?: IterableEmbeddedMessageDefaultAction;
+  buttons?: IterableEmbeddedMessageButton[];
+  text?: IterableEmbeddedMessageText[];
 }
