1e2a3a96609415fed67e32483db5e5fa7ff5ec3c
Evan Greer <evan.greer@evan.greer>
2025-05-27 16:56:32 -0600

    feat: adds dictionary processing to IterableEmbeddedPlacement and IterableEmbeddedMessage

--- src/embedded/classes/IterableEmbeddedMessage.ts
+++ src/embedded/classes/IterableEmbeddedMessage.ts
@@ -3,16 +3,34 @@ import { IterableEmbeddedMessageElements } from './IterableEmbeddedMessageElemen
 
 export class IterableEmbeddedMessage {
   metadata: IterableEmbeddedMessageMetadata;
-  elements: IterableEmbeddedMessageElements;
-  payload: Record<string, unknown>;
+  elements?: IterableEmbeddedMessageElements;
+  payload?: Record<string, unknown>;
 
   constructor(
     metadata: IterableEmbeddedMessageMetadata,
-    elements: IterableEmbeddedMessageElements,
-    payload: Record<string, unknown>
+    elements?: IterableEmbeddedMessageElements,
+    payload?: Record<string, unknown>
   ) {
     this.metadata = metadata;
     this.elements = elements;
     this.payload = payload;
   }
+
+  static fromDict(dict: Partial<EmbeddedMessageDict>): IterableEmbeddedMessage {
+    if (!dict.metadata) {
+      throw new Error('metadata is required');
+    }
+    const metadata = IterableEmbeddedMessageMetadata.fromDict(dict.metadata);
+    const elements = dict.elements
+      ? IterableEmbeddedMessageElements.fromDict(dict.elements)
+      : undefined;
+    const payload = dict.payload;
+    return new IterableEmbeddedMessage(metadata, elements, payload);
+  }
+}
+
+interface EmbeddedMessageDict {
+  metadata: IterableEmbeddedMessageMetadata;
+  elements: IterableEmbeddedMessageElements;
+  payload: Record<string, unknown>;
 }
--- src/embedded/classes/IterableEmbeddedPlacement.ts
+++ src/embedded/classes/IterableEmbeddedPlacement.ts
@@ -7,10 +7,31 @@ import { IterableEmbeddedMessage } from './IterableEmbeddedMessage';
 export class IterableEmbeddedPlacement {
   readonly placementId: number;
 
-  readonly messages: IterableEmbeddedMessage[];
+  readonly messages?: IterableEmbeddedMessage[];
 
-  constructor(placementId: number, messages: IterableEmbeddedMessage[]) {
+  constructor(placementId: number, messages?: IterableEmbeddedMessage[]) {
     this.placementId = placementId;
     this.messages = messages;
   }
+
+  static fromDict(
+    dict: Partial<EmbeddedPlacementDict>
+  ): IterableEmbeddedPlacement {
+    if (!dict.placementId) {
+      throw new Error('placementId is required');
+    }
+
+    const placementId = dict.placementId;
+    const messages = dict.messages
+      ? dict.messages?.map((message) =>
+          IterableEmbeddedMessage.fromDict(message)
+        )
+      : undefined;
+    return new IterableEmbeddedPlacement(placementId, messages);
+  }
+}
+
+interface EmbeddedPlacementDict {
+  placementId: number;
+  messages: IterableEmbeddedMessage[];
 }
