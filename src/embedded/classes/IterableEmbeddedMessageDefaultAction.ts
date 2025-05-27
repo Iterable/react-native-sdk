78c92f8cb6c7439e4104b945d1c27380db136f94
Evan Greer <evan.greer@evan.greer>
2025-05-27 12:46:13 -0600

    feat: adds docs to IterableEmbeddedMessageDefaultAction

--- src/embedded/classes/IterableEmbeddedMessageDefaultAction.ts
+++ src/embedded/classes/IterableEmbeddedMessageDefaultAction.ts
@@ -1,12 +1,33 @@
 export class IterableEmbeddedMessageDefaultAction {
+  /**
+   * The type of iterable action
+   * For custom actions, the type is `action://` prefix followed by a custom action name
+   */
   readonly type: string;
+
+  /**
+   * The url for the action when the type is `openUrl`
+   * For custom actions, data is empty
+   */
   readonly data?: string;
 
+  /**
+   * Creates an instance of `IterableEmbeddedMessageDefaultAction`.
+   *
+   * @param type - The type of iterable action
+   * @param data - The url for the action when the type is `openUrl`
+   */
   constructor(type: string, data?: string) {
     this.type = type;
     this.data = data;
   }
 
+  /**
+   * Creates an instance of `IterableEmbeddedMessageDefaultAction` from a dictionary object.
+   *
+   * @param dict - The dictionary object containing the properties to initialize the `IterableEmbeddedMessageDefaultAction` instance.
+   * @returns A new instance of `IterableEmbeddedMessageDefaultAction` initialized with the provided dictionary properties.
+   */
   static fromDict(
     dict: Partial<EmbeddedMessageDefaultActionDict>
   ): IterableEmbeddedMessageDefaultAction {
@@ -17,6 +38,9 @@ export class IterableEmbeddedMessageDefaultAction {
   }
 }
 
+/**
+ * An interface defining the dictionary object containing the properties for the embedded message default action.
+ */
 export interface EmbeddedMessageDefaultActionDict {
   type: string;
   data?: string;
