54a04e010bdfef3003db7f9fe1730065a8a6a815
Evan Greer <evan.greer@evan.greer>
2025-06-04 09:57:35 -0600

    feat: creates IterableEmbeddedPlacement and IterableEmbeddedMessage classes

--- src/embedded/classes/IterableEmbeddedPlacement.ts
+++ src/embedded/classes/IterableEmbeddedPlacement.ts
@@ -1,11 +1,52 @@
+import { IterableEmbeddedMessage } from './IterableEmbeddedMessage';
+
 /**
- * Iterable embedded placement
- * Contains placement id and the associated embedded messages
+ * IterableEmbeddedPlacement represents an embedded placement.
  */
 export class IterableEmbeddedPlacement {
+  /** The placement id of the embedded placement */
   readonly placementId: number;
+  /** The messages associated with the embedded placement */
+  readonly messages?: IterableEmbeddedMessage[];
 
-  constructor(placementId: number) {
+  /**
+   * Creates an instance of `IterableEmbeddedPlacement`.
+   *
+   * @param placementId - The placement id of the embedded placement.
+   * @param messages - The messages associated with the embedded placement.
+   */
+  constructor(placementId: number, messages?: IterableEmbeddedMessage[]) {
     this.placementId = placementId;
+    this.messages = messages;
   }
+
+  /**
+   * Creates an instance of `IterableEmbeddedPlacement` from a dictionary object.
+   *
+   * @param dict - The dictionary object containing the properties to initialize the `IterableEmbeddedPlacement` instance.
+   * @returns A new instance of `IterableEmbeddedPlacement` initialized with the provided dictionary properties.
+   */
+  static fromDict(
+    dict: Partial<EmbeddedPlacementDict>
+  ): IterableEmbeddedPlacement {
+    if (!dict.placementId) {
+      throw new Error('placementId is required');
+    }
+
+    const placementId = dict.placementId;
+    const messages = dict.messages
+      ? dict.messages?.map((message) =>
+          IterableEmbeddedMessage.fromDict(message)
+        )
+      : undefined;
+    return new IterableEmbeddedPlacement(placementId, messages);
+  }
+}
+
+/**
+ * An interface defining the dictionary object containing the properties for the embedded placement.
+ */
+export interface EmbeddedPlacementDict {
+  placementId: number;
+  messages?: IterableEmbeddedMessage[];
 }
