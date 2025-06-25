0abea2a3aaa53ace635466cada6fc6e339e9e4f2
Loren Posen <loren.posen@iterable.com>
2025-06-25 12:48:51 -0700

    feat: add iOS configuration and React Native CLI dependency for improved project setup

---
+++ react-native.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  project: {
+    ios: {
+      automaticPodsInstallation: true,
+    },
+    android: {},
+  },
+  dependencies: {
+    '@iterable/react-native-sdk': {
+      root: __dirname,
+      platforms: {
+        ios: {},
+        android: {},
+      },
+    },
+  },
+};
