76e8fa1a4f42fcc606f585b9260ff688ee8652b7
Loren Posen <loren.posen@iterable.com>
2025-07-22 11:55:16 -0700

    refactor: comment out unused methods in NativeRNIterableAPI interface for clarity

--- src/api/NativeRNIterableAPI.ts
+++ src/api/NativeRNIterableAPI.ts
@@ -21,7 +21,7 @@ export interface IterableConfigDict {
 
 export interface Spec extends TurboModule {
   // multiply(a: number, b: number): number;
-  hello(): void;
+  // hello(): void;
 
   // Initialization
   // initializeWithApiKey(
@@ -30,126 +30,126 @@ export interface Spec extends TurboModule {
   //   version: string
   // ): Promise<boolean>;
   // testing change
-  initializeWithApiKey(
-    apiKey: string,
-    config: IterableConfigDict,
-    version: string
-  ): void;
+  // initializeWithApiKey(
+  //   apiKey: string,
+  //   config: IterableConfigDict,
+  //   version: string
+  // ): void;
 
-  initialize2WithApiKey(
-    apiKey: string,
-    config: IterableConfigDict,
-    apiEndPointOverride: string,
-    version: string
-  ): Promise<boolean>;
+  // initialize2WithApiKey(
+  //   apiKey: string,
+  //   config: IterableConfigDict,
+  //   apiEndPointOverride: string,
+  //   version: string
+  // ): Promise<boolean>;
 
-  // User management
-  setEmail(email: string | null, authToken?: string | null): void;
-  getEmail(): Promise<string | null>;
-  setUserId(userId: string | null, authToken?: string | null): void;
-  getUserId(): Promise<string | null>;
+  // // User management
+  // setEmail(email: string | null, authToken?: string | null): void;
+  // getEmail(): Promise<string | null>;
+  // setUserId(userId: string | null, authToken?: string | null): void;
+  // getUserId(): Promise<string | null>;
 
-  // In-app messaging
-  setInAppShowResponse(number: number): void;
-  getInAppMessages(): Promise<{ [key: string]: string | number | boolean }[]>;
-  getInboxMessages(): Promise<{ [key: string]: string | number | boolean }[]>;
-  getUnreadInboxMessagesCount(): Promise<number>;
-  showMessage(messageId: string, consume: boolean): Promise<string | null>;
-  removeMessage(messageId: string, location: number, source: number): void;
-  setReadForMessage(messageId: string, read: boolean): void;
-  setAutoDisplayPaused(autoDisplayPaused: boolean): void;
+  // // In-app messaging
+  // setInAppShowResponse(number: number): void;
+  // getInAppMessages(): Promise<{ [key: string]: string | number | boolean }[]>;
+  // getInboxMessages(): Promise<{ [key: string]: string | number | boolean }[]>;
+  // getUnreadInboxMessagesCount(): Promise<number>;
+  // showMessage(messageId: string, consume: boolean): Promise<string | null>;
+  // removeMessage(messageId: string, location: number, source: number): void;
+  // setReadForMessage(messageId: string, read: boolean): void;
+  // setAutoDisplayPaused(autoDisplayPaused: boolean): void;
 
-  // Tracking
-  trackEvent(
-    name: string,
-    dataFields?: { [key: string]: string | number | boolean }
-  ): void;
-  trackPushOpenWithCampaignId(
-    campaignId: number,
-    templateId: number | null,
-    messageId: string,
-    appAlreadyRunning: boolean,
-    dataFields?: { [key: string]: string | number | boolean }
-  ): void;
-  trackInAppOpen(messageId: string, location: number): void;
-  trackInAppClick(
-    messageId: string,
-    location: number,
-    clickedUrl: string
-  ): void;
-  trackInAppClose(
-    messageId: string,
-    location: number,
-    source: number,
-    clickedUrl?: string
-  ): void;
-  inAppConsume(messageId: string, location: number, source: number): void;
+  // // Tracking
+  // trackEvent(
+  //   name: string,
+  //   dataFields?: { [key: string]: string | number | boolean }
+  // ): void;
+  // trackPushOpenWithCampaignId(
+  //   campaignId: number,
+  //   templateId: number | null,
+  //   messageId: string,
+  //   appAlreadyRunning: boolean,
+  //   dataFields?: { [key: string]: string | number | boolean }
+  // ): void;
+  // trackInAppOpen(messageId: string, location: number): void;
+  // trackInAppClick(
+  //   messageId: string,
+  //   location: number,
+  //   clickedUrl: string
+  // ): void;
+  // trackInAppClose(
+  //   messageId: string,
+  //   location: number,
+  //   source: number,
+  //   clickedUrl?: string
+  // ): void;
+  // inAppConsume(messageId: string, location: number, source: number): void;
 
-  // Commerce
-  updateCart(items: { [key: string]: string | number | boolean }[]): void;
-  trackPurchase(
-    total: number,
-    items: { [key: string]: string | number | boolean }[],
-    dataFields?: { [key: string]: string | number | boolean }
-  ): void;
+  // // Commerce
+  // updateCart(items: { [key: string]: string | number | boolean }[]): void;
+  // trackPurchase(
+  //   total: number,
+  //   items: { [key: string]: string | number | boolean }[],
+  //   dataFields?: { [key: string]: string | number | boolean }
+  // ): void;
 
-  // User data
-  updateUser(
-    dataFields: { [key: string]: string | number | boolean },
-    mergeNestedObjects: boolean
-  ): void;
-  updateEmail(email: string, authToken?: string): void;
+  // // User data
+  // updateUser(
+  //   dataFields: { [key: string]: string | number | boolean },
+  //   mergeNestedObjects: boolean
+  // ): void;
+  // updateEmail(email: string, authToken?: string): void;
 
-  // Attribution
-  getAttributionInfo(): Promise<{
-    [key: string]: string | number | boolean;
-  } | null>;
-  setAttributionInfo(
-    dict: { [key: string]: string | number | boolean } | null
-  ): void;
+  // // Attribution
+  // getAttributionInfo(): Promise<{
+  //   [key: string]: string | number | boolean;
+  // } | null>;
+  // setAttributionInfo(
+  //   dict: { [key: string]: string | number | boolean } | null
+  // ): void;
 
-  // Device management
-  disableDeviceForCurrentUser(): void;
-  getLastPushPayload(): Promise<{
-    [key: string]: string | number | boolean;
-  } | null>;
+  // // Device management
+  // disableDeviceForCurrentUser(): void;
+  // getLastPushPayload(): Promise<{
+  //   [key: string]: string | number | boolean;
+  // } | null>;
 
-  // Content
-  getHtmlInAppContentForMessage(
-    messageId: string
-  ): Promise<{ [key: string]: string | number | boolean }>;
+  // // Content
+  // getHtmlInAppContentForMessage(
+  //   messageId: string
+  // ): Promise<{ [key: string]: string | number | boolean }>;
 
-  // App links
-  handleAppLink(appLink: string): Promise<boolean>;
+  // // App links
+  // handleAppLink(appLink: string): Promise<boolean>;
 
-  // Subscriptions
-  updateSubscriptions(
-    emailListIds: number[] | null,
-    unsubscribedChannelIds: number[] | null,
-    unsubscribedMessageTypeIds: number[] | null,
-    subscribedMessageTypeIds: number[] | null,
-    campaignId: number,
-    templateId: number
-  ): void;
+  // // Subscriptions
+  // updateSubscriptions(
+  //   emailListIds: number[] | null,
+  //   unsubscribedChannelIds: number[] | null,
+  //   unsubscribedMessageTypeIds: number[] | null,
+  //   subscribedMessageTypeIds: number[] | null,
+  //   campaignId: number,
+  //   templateId: number
+  // ): void;
 
-  // Session tracking
-  startSession(
-    visibleRows: { [key: string]: string | number | boolean }[]
-  ): void;
-  endSession(): void;
-  updateVisibleRows(
-    visibleRows: { [key: string]: string | number | boolean }[]
-  ): void;
+  // // Session tracking
+  // startSession(
+  //   visibleRows: { [key: string]: string | number | boolean }[]
+  // ): void;
+  // endSession(): void;
+  // updateVisibleRows(
+  //   visibleRows: { [key: string]: string | number | boolean }[]
+  // ): void;
 
-  // Auth
-  passAlongAuthToken(authToken: string | null): void;
+  // // Auth
+  // passAlongAuthToken(authToken: string | null): void;
 
-  // Wake app -- android only
-  wakeApp(): void;
+  // // Wake app -- android only
+  // wakeApp(): void;
 
-  // If your module will emit events, include the required listener stubs:
-  addListener(eventName: string): void;
-  removeListeners(count: number): void;
+  // // If your module will emit events, include the required listener stubs:
+  // addListener(eventName: string): void;
+  // removeListeners(count: number): void;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('RNIterableAPI');
