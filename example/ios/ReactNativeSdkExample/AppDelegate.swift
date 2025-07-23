6babee91bcbe2f08798ee5308d17a233e428f1cf
Loren Posen <loren.posen@iterable.com>
2025-07-22 21:56:38 -0700

    feat: enhance AppDelegate with push notification permissions, deep link support, and user notification handling for Iterable integration

--- example/ios/ReactNativeSdkExample/AppDelegate.swift
+++ example/ios/ReactNativeSdkExample/AppDelegate.swift
@@ -38,34 +38,85 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
       launchOptions: launchOptions
     )
 
-    // setupUserNotificationCenter()
+    UNUserNotificationCenter.current().delegate = self
+
+    /**
+    * Request permissions for push notifications.
+    * @see Step 3.5.5 of https://support.iterable.com/hc/en-us/articles/360045714132-Installing-Iterable-s-React-Native-SDK#step-3-5-set-up-support-for-push-notifications
+    */
+    requestPushPermissions()
 
     return true
   }
 
-  func application(
-    _ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+  /**
+    * Add support for in-app messages
+    * @see Step 3.6 of https://support.iterable.com/hc/en-us/articles/360045714132-Installing-Iterable-s-React-Native-SDK#step-3-6-add-support-for-in-app-messages
+    */
+  public func application(
+    _ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+    fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void
   ) {
-    NSLog("didRegisterForRemoteNotificationsWithDeviceToken: \(deviceToken)")
-    // ReactIterableAPI.register(token: deviceToken)
+
+    // IterableAppIntegration.application(
+    //   application, didReceiveRemoteNotification: userInfo,
+    //   fetchCompletionHandler: completionHandler
+    // )
   }
 
-  // private func setupUserNotificationCenter() {
-  //   UNUserNotificationCenter.current().delegate = self
-  //   UNUserNotificationCenter.current().getNotificationSettings { (settings) in
-  //     if settings.authorizationStatus != .authorized {
-  //       NSLog("Not authorized")
-  //       // not authorized, ask for permission
-  //       UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) {
-  //         (success, error) in
-  //         NSLog("auth: \(success)")
-  //       }
-  //     } else {
-  //       // already authorized
-  //       NSLog("Already authorized")
-  //     }
-  //   }
-  // }
+  public func application(
+    _ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+  ) {
+    /**
+     * Register the device token with Iterable.
+     * @see Step 3.5.4 of https://support.iterable.com/hc/en-us/articles/360045714132-Installing-Iterable-s-React-Native-SDK#step-3-5-set-up-support-for-push-notifications
+     */
+    // IterableAPI.register(token: deviceToken)
+  }
+
+  /**
+   * Add support for deep links
+   * @see Step 3.7 of https://support.iterable.com/hc/en-us/articles/360045714132-Installing-Iterable-s-React-Native-SDK#step-3-7-add-support-for-deep-links
+   */
+  public func application(
+    _ application: UIApplication,
+    continue userActivity: NSUserActivity,
+    restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void
+  ) -> Bool {
+    return RCTLinkingManager.application(
+      application,
+      continue: userActivity,
+      restorationHandler: restorationHandler
+    )
+  }
+
+  /**
+    * Add support for deep links
+    * @see Step 3.7 of https://support.iterable.com/hc/en-us/articles/360045714132-Installing-Iterable-s-React-Native-SDK#step-3-7-add-support-for-deep-links
+    */
+  public func application(
+    _ app: UIApplication,
+    open url: URL,
+    options: [UIApplication.OpenURLOptionsKey: Any] = [:]
+  ) -> Bool {
+    RCTLinkingManager.application(app, open: url, options: options)
+  }
+
+  public func requestPushPermissions() {
+    UNUserNotificationCenter.current().getNotificationSettings { (settings) in
+      if settings.authorizationStatus != .authorized {
+        NSLog("Not authorized")
+        // not authorized, ask for permission
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) {
+          (success, error) in
+          NSLog("auth: \(success)")
+        }
+      } else {
+        // already authorized
+        NSLog("Already authorized")
+      }
+    }
+  }
 }
 
 class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
@@ -79,5 +130,26 @@ class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
     #else
       Bundle.main.url(forResource: "main", withExtension: "jsbundle")
     #endif
+  }
+}
+
+/// * Handle incoming push notifications and enable push notification tracking.
+/// * @see Step 3.5.5 of https://support.iterable.com/hc/en-us/articles/360045714132-Installing-Iterable-s-React-Native-SDK#step-3-5-set-up-support-for-push-notifications
+extension AppDelegate: UNUserNotificationCenterDelegate {
+  // App is running in the foreground
+  public func userNotificationCenter(
+    _ center: UNUserNotificationCenter, willPresent notification: UNNotification,
+    withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+  ) {
+    completionHandler([.alert, .badge, .sound, .banner])
+  }
+
+  // The method will be called on the delegate when the user responded to the notification by opening the application, dismissing the notification or choosing a UNNotificationAction. The delegate must be set before the application returns from applicationDidFinishLaunching:.
+  public func userNotificationCenter(
+    _ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse,
+    withCompletionHandler completionHandler: @escaping () -> Void
+  ) {
+    // IterableAppIntegration.userNotificationCenter(
+    //   center, didReceive: response, withCompletionHandler: completionHandler)
   }
 }
