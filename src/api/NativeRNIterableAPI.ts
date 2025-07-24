d197dc55e3b0445e1c474470b22b835dd2471e3d
Loren Posen <loren.posen@iterable.com>
2025-07-24 12:19:23 -0700

    chore: type fixes

--- src/api/NativeRNIterableAPI.ts
+++ src/api/NativeRNIterableAPI.ts
@@ -1,5 +1,5 @@
 import type { TurboModule } from 'react-native';
-import { NativeModules, TurboModuleRegistry } from 'react-native';
+import { TurboModuleRegistry } from 'react-native';
 
 export interface Spec extends TurboModule {
   // Initialization
