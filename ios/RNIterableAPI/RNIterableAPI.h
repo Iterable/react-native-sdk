5f8fd9ffc27af83710dd1c0b5224b4f893046b29
Loren Posen <loren.posen@iterable.com>
2025-08-02 09:58:45 -0700

    refactor: remove legacy architecture code to reduce PR bloat

--- ios/RNIterableAPI/RNIterableAPI.h
+++ ios/RNIterableAPI/RNIterableAPI.h
@@ -1,20 +1,10 @@
 #import <Foundation/Foundation.h>
 #import <React/RCTEventEmitter.h>
-
-#if RCT_NEW_ARCH_ENABLED
-
-  #import <React/RCTUtils.h>
-  #import <React/RCTConvert.h>
-  #import <React/RCTTurboModuleRegistry.h>
-  #import <RNIterableAPISpec/RNIterableAPISpec.h>
+#import <React/RCTUtils.h>
+#import <React/RCTConvert.h>
+#import <React/RCTTurboModuleRegistry.h>
+#import <RNIterableAPISpec/RNIterableAPISpec.h>
 
 @interface RNIterableAPI : RCTEventEmitter <NativeRNIterableAPISpec>
-
-#else
-
-  #import <React/RCTBridgeModule.h>
-@interface RNIterableAPI : RCTEventEmitter <RCTBridgeModule>
-
-#endif
 
 @end
