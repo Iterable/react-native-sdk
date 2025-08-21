d00e31d78d2b1bf900c05bca27bbbcfc44ca3aed
Loren Posen <loren.posen@iterable.com>
2025-08-21 15:38:37 -0700

    refactor: split packages between old and new architecture

--- android/src/main/java/com/iterable/reactnative/RNIterableAPIPackage.java
+++ android/src/main/java/com/iterable/reactnative/RNIterableAPIPackage.java
@@ -3,27 +3,49 @@ package com.iterable.reactnative;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import com.facebook.react.ReactPackage;
+import androidx.annotation.Nullable;
+import com.facebook.react.bridge.JavaScriptModule; // TODO: instructrions says to remove
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.module.model.ReactModuleInfo;
+import com.facebook.react.module.model.ReactModuleInfoProvider;
+import com.facebook.react.ReactPackage; // TODO: instructrions says to remove
+import com.facebook.react.TurboReactPackage;
 import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
 
-public class RNIterableAPIPackage implements ReactPackage {
+
+public class RNIterableAPIPackage extends TurboReactPackage {
+    @Nullable
     @Override
-    public List<NativeModule> createNativeModules(
-            ReactApplicationContext reactContext) {
-        List<NativeModule> modules = new ArrayList<>();
-
-        modules.add(new RNIterableAPIModule(reactContext));
-
-        return modules;
+    public NativeModule getModule(String name, ReactApplicationContext reactContext) {
+      if (name.equals(RNIterableAPIModuleImpl.NAME)) {
+          return new RNIterableAPIModule(reactContext);
+      } else {
+          return null;
+      }
     }
 
     @Override
-    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-        return Collections.emptyList();
+    public ReactModuleInfoProvider getReactModuleInfoProvider() {
+        return () -> {
+            final Map<String, ReactModuleInfo> moduleInfos = new HashMap<>();
+            boolean isTurboModule = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+            moduleInfos.put(
+              RNIterableAPIModuleImpl.NAME,
+              new ReactModuleInfo(
+                  RNIterableAPIModuleImpl.NAME,
+                  RNIterableAPIModuleImpl.NAME,
+                  false, // canOverrideExistingModule
+                  false, // needsEagerInit
+                  true, // hasConstants
+                  false, // isCxxModule
+                  isTurboModule // isTurboModule
+            ));
+            return moduleInfos;
+        };
     }
 }
