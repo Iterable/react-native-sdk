356e76784c55ce51daf8df9bff0738e301732d65
Loren Posen <loren.posen@iterable.com>
2025-10-06 22:26:02 -0700

    feat: implement user notification handling and remote notification registration in AppDelegate

--- example/ios/ReactNativeSdkExample/AppDelegate.swift
+++ example/ios/ReactNativeSdkExample/AppDelegate.swift
@@ -5,10 +5,13 @@
 //  Created by Loren Posen on 6/11/25.
 //
 
-import React
-import ReactAppDependencyProvider
-import React_RCTAppDelegate
 import UIKit
+import React
+import React_RCTAppDelegate
+import ReactAppDependencyProvider
+import UserNotifications
+
+import IterableSDK
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -21,6 +24,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
   ) -> Bool {
+    NSLog("FINISHED LAUNCHING WITH OPTIONS")
+    ITBInfo()
+
     let delegate = ReactNativeDelegate()
     let factory = RCTReactNativeFactory(delegate: delegate)
     delegate.dependencyProvider = RCTAppDependencyProvider()
@@ -36,7 +42,72 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
       launchOptions: launchOptions
     )
 
+    setupUserNotificationCenter()
+
     return true
+  }
+
+  func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    NSLog("REGISTERED FOR REMOTE NOTIFICATIONS")
+    ITBInfo()
+    IterableAPI.register(token: deviceToken)
+  }
+
+  func application(_ application: UIApplication,
+                 didFailToRegisterForRemoteNotificationsWithError
+                 error: Error) {
+    NSLog("FAILED TO REGISTER FOR REMOTE NOTIFICATIONS")
+    ITBInfo("error: \(error)")
+  }
+
+  func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+    NSLog("RECEIVED REMOTE NOTIFICATIONS")
+    ITBInfo()
+    IterableAppIntegration.application(application, didReceiveRemoteNotification: userInfo, fetchCompletionHandler: completionHandler)
+  }
+
+  func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+    NSLog("RECEIVED UNIVERSAL LINK")
+    NSLog("userActivity: \(userActivity)")
+    ITBInfo()
+    guard let url = userActivity.webpageURL else {
+        return false
+    }
+
+    return IterableAPI.handle(universalLink: url)
+  }
+
+  func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+      NSLog("OPEN URL")
+      NSLog("url: \(url)")
+      NSLog("options: \(options)")
+      ITBInfo()
+      return RCTLinkingManager.application(app, open: url, options: options)
+  }
+
+  private func setupUserNotificationCenter() {
+    UNUserNotificationCenter.current().delegate = self
+    UNUserNotificationCenter.current().getNotificationSettings { settings in
+      if settings.authorizationStatus != .authorized {
+        ITBInfo("Not authorized")
+        // not authorized, ask for permission
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, _ in
+          ITBInfo("auth: \(success)")
+          if success {
+            DispatchQueue.main.async {
+              UIApplication.shared.registerForRemoteNotifications()
+            }
+          }
+          // TODO: Handle error etc.
+        }
+      } else {
+        // already authorized
+        ITBInfo("Already authorized")
+        DispatchQueue.main.async {
+          UIApplication.shared.registerForRemoteNotifications()
+        }
+      }
+    }
   }
 }
 
@@ -46,10 +117,24 @@ class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
   }
 
   override func bundleURL() -> URL? {
-    #if DEBUG
-      RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
-    #else
-      Bundle.main.url(forResource: "main", withExtension: "jsbundle")
-    #endif
+#if DEBUG
+    RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+#else
+    Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+#endif
+  }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+  // App is running in the foreground
+  public func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+    NSLog("WILL PRESENT NOTIFICATION")
+    completionHandler([.alert, .badge, .sound])
+  }
+
+  // The method will be called on the delegate when the user responded to the notification by opening the application, dismissing the notification or choosing a UNNotificationAction. The delegate must be set before the application returns from applicationDidFinishLaunching:.
+  public func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+    NSLog("DID RECEIVE NOTIFICATION RESPONSE")
+    IterableAppIntegration.userNotificationCenter(center, didReceive: response, withCompletionHandler: completionHandler)
   }
 }
