bd32975ffb7b2db64ed2052f78caf89359eb3f0d
Loren Posen <loren.posen@iterable.com>
2025-10-06 19:10:33 -0700

    feat: add authentication failure and retry policy enums and types

---
+++ src/core/enums/IterableRetryBackoff.ts
@@ -0,0 +1,15 @@
+/**
+ * The type of backoff to use when retrying a request.
+ */
+export enum IterableRetryBackoff {
+  /**
+   * Linear backoff (each retry will wait for a fixed interval)
+   * TODO: check with @Ayyanchira if this is correct
+   */
+  LINEAR = 'LINEAR',
+  /**
+   * Exponential backoff (each retry will wait for an interval that increases exponentially)
+   * TODO: check with @Ayyanchira if this is correct
+   */
+  EXPONENTIAL = 'EXPONENTIAL',
+}
