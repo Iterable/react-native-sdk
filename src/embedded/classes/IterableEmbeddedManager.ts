9940f46b1f4c8cec2fe6252e1dc5a0a6bb3e07ea
Loren Posen <loren.posen@iterable.com>
2025-10-10 20:54:10 -0700

    docs: enhance documentation

--- src/embedded/classes/IterableEmbeddedManager.ts
+++ src/embedded/classes/IterableEmbeddedManager.ts
@@ -1,6 +1,9 @@
 /**
  * Manages embedded messages from Iterable.
  *
+ * Provides embedded message functionality including retrieving messages,
+ * displaying messages, removing messages, and more.
+ *
  * **Documentation**
  * - [Embedded Messaging Overview](https://support.iterable.com/hc/en-us/articles/23060529977364-Embedded-Messaging-Overview)
  * - [Android Embedded Messaging](https://support.iterable.com/hc/en-us/articles/23061877893652-Embedded-Messages-with-Iterable-s-Android-SDK)
