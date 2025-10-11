789ae831cebed8d79dc4917850e9134ea6564350
Loren Posen <loren.posen@iterable.com>
2025-10-10 20:48:10 -0700

    refactor: remove unused methods and improve documentation in IterableEmbeddedManager

--- src/embedded/classes/IterableEmbeddedManager.ts
+++ src/embedded/classes/IterableEmbeddedManager.ts
@@ -1,66 +1,14 @@
-import { IterableLogger } from '../../core/classes/IterableLogger';
-
 /**
- * Manages the embedded messages from Iterable.
+ * Manages embedded messages from Iterable.
+ *
+ * **Documentation**
+ * - [Embedded Messaging Overview](https://support.iterable.com/hc/en-us/articles/23060529977364-Embedded-Messaging-Overview)
+ * - [Android Embedded Messaging](https://support.iterable.com/hc/en-us/articles/23061877893652-Embedded-Messages-with-Iterable-s-Android-SDK)
+ * - [iOS Embedded Messaging](https://support.iterable.com/hc/en-us/articles/23061840746900-Embedded-Messages-with-Iterable-s-iOS-SDK)
  */
 export class IterableEmbeddedManager {
   /**
-   * Refreshes the local cache of your embedded manager system so that it aligns
-   * with the server.
-   *
-   * At key points during your app's lifecycle, you may want to manually refresh
-   * your app's local cache of embedded messages. For example, as users navigate
-   * around, on pull-to-refresh, etc.
-   *
-   * @returns A promise that returns messages that the user is *eligible* to see.
-   *
-   * @example
-   * ```typescript
-   * IterableEmbeddedManager.syncMessages().then(messages => {
-   *   console.log('Messages:', messages);
-   * });
-   * ```
+   * Whether the embedded manager is enabled.
    */
-  syncMessages() {
-    IterableLogger.log('IterableEmbeddedManager.syncMessages');
-  }
-
-  getMessages(placementIds?: number[] | null) {
-    IterableLogger.log(
-      'IterableEmbeddedManager.getMessages with placementIds',
-      placementIds
-    );
-  }
-
-  getPlacementIds() {
-    IterableLogger.log('IterableEmbeddedManager.getEmbeddedPlacementIds');
-  }
-
-  startSession() {
-    IterableLogger.log('IterableEmbeddedManager.startSession');
-  }
-
-  endSession() {
-    IterableLogger.log('IterableEmbeddedManager.endSession');
-  }
-
-  startImpression(messageId: string, placementId: number) {
-    IterableLogger.log(
-      'IterableEmbeddedManager.startImpression',
-      messageId,
-      placementId
-    );
-  }
-
-  pauseImpression(messageId: string) {
-    IterableLogger.log('IterableEmbeddedManager.pauseImpression', messageId);
-  }
-
-  handleClick() {
-    IterableLogger.log('IterableEmbeddedManager.handleClick');
-  }
-
-  trackClick() {
-    IterableLogger.log('IterableEmbeddedManager.trackClick');
-  }
+  isEnabled = false;
 }
