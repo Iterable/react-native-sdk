11a4cfb915b93fe10c9a4be87da61546d0d2b1e3
Loren Posen <loren.posen@iterable.com>
2025-10-10 23:37:03 -0700

    feat: add embedded message types

---
+++ src/embedded/types/IterableEmbeddedMessageElementsButton.ts
@@ -0,0 +1,10 @@
+import type { IterableAction } from '../../core/classes/IterableAction';
+
+export interface IterableEmbeddedMessageElementsButton {
+  /** The ID. */
+  id: string;
+  /** The title. */
+  title?: string | null;
+  /** The associated action. */
+  action?: IterableAction | null;
+}
