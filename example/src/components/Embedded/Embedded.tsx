b0e17f99db47c540f2ab66babb7062bcca348bf8
Loren Posen <loren.posen@iterable.com>
2025-10-10 22:25:45 -0700

    feat: add functionality to retrieve and display placement IDs in Embedded component

--- example/src/components/Embedded/Embedded.tsx
+++ example/src/components/Embedded/Embedded.tsx
@@ -1,9 +1,21 @@
-import { Text, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { useCallback, useState } from 'react';
 import { Iterable } from '@iterable/react-native-sdk';
 
 import styles from './Embedded.styles';
 
 export const Embedded = () => {
+  const [placementIds, setPlacementIds] = useState<number[]>([]);
+  const getPlacementIds = useCallback(() => {
+    Iterable.embeddedManager.getPlacementIds().then((ids: unknown) => {
+      console.log(ids);
+      setPlacementIds(ids as number[]);
+    });
+    // .catch((error) => {
+    //   console.error(error);
+    // });
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>EMBEDDED</Text>
@@ -14,6 +26,12 @@ export const Embedded = () => {
         Is embedded manager enabled?
         {Iterable.embeddedManager.isEnabled ? 'Yes' : 'No'}
       </Text>
+      <Text style={styles.text}>
+        Placement ids: [{placementIds.join(', ')}]
+      </Text>
+      <TouchableOpacity style={styles.button} onPress={getPlacementIds}>
+        <Text style={styles.buttonText}>Get placement ids</Text>
+      </TouchableOpacity>
     </View>
   );
 };
