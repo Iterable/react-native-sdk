8201c4da1e98bdf60e657e419940f33a3bb446d6
Loren Posen <loren.posen@iterable.com>
2025-10-10 22:16:56 -0700

    feat: display embedded manager status in Embedded component

--- example/src/components/Embedded/Embedded.tsx
+++ example/src/components/Embedded/Embedded.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from 'react-native';
+import { Iterable } from '@iterable/react-native-sdk';
 
 import styles from './Embedded.styles';
 
@@ -6,6 +7,13 @@ export const Embedded = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>EMBEDDED</Text>
+      <Text style={styles.text}>
+        Does embedded class exist? {Iterable.embeddedManager ? 'Yes' : 'No'}
+      </Text>
+      <Text style={styles.text}>
+        Is embedded manager enabled?
+        {Iterable.embeddedManager.isEnabled ? 'Yes' : 'No'}
+      </Text>
     </View>
   );
 };
