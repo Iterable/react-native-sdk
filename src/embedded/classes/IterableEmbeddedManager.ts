a6f813990c9a2e84e68765d0ea46085a2d2564a9
Loren Posen <loren.posen@iterable.com>
2025-10-10 23:19:23 -0700

    feat: add sync and getMessages methods

--- src/embedded/classes/IterableEmbeddedManager.ts
+++ src/embedded/classes/IterableEmbeddedManager.ts
@@ -1,4 +1,5 @@
 import { IterableApi } from '../../core/classes/IterableApi';
+import { IterableLogger } from '../../core/classes/IterableLogger';
 
 /**
  * Manages embedded messages from Iterable.
@@ -18,11 +19,49 @@ export class IterableEmbeddedManager {
   isEnabled = false;
 
   /**
+   * Refreshes the local cache of your embedded manager system so that it aligns
+   * with the server.
+   *
+   * You may want to manually refresh your app's local cache of embedded
+   * messages during key points during your app's lifecycle. For example, as
+   * users navigate around, on pull-to-refresh, etc.
+   *
+   * @example
+   * ```typescript
+   * IterableEmbeddedManager.syncMessages().then(messages => {
+   *   console.log('Messages:', messages);
+   * });
+   * ```
+   */
+  sync() {
+    IterableLogger.log('IterableEmbeddedManager.syncMessages');
+  }
+
+  /**
    * Retrieves a list of placement IDs for the embedded manager.
    *
    * [Placement Documentation](https://support.iterable.com/hc/en-us/articles/23060529977364-Embedded-Messaging-Overview#placements-and-prioritization)
    */
   getPlacementIds() {
     return IterableApi.getEmbeddedPlacementIds();
+  }
+
+  /**
+   * Retrieves the messages that the user is *eligible* to see.
+   *
+   * @param placementIds - The placement IDs to retrieve messages for.
+   *
+   * @example
+   * ```typescript
+   * IterableEmbeddedManager.getMessages().then(messages => {
+   *   console.log('Messages:', messages);
+   * });
+   * ```
+   */
+  getMessages(placementIds?: number[] | null) {
+    IterableLogger.log(
+      'IterableEmbeddedManager.getMessages with placementIds',
+      placementIds
+    );
   }
 }
