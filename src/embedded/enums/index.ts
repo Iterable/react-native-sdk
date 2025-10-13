3cae7b428ee92d9947669b5fd20b3d8c0f0efcf6
Loren Posen <loren.posen@iterable.com>
2025-10-13 16:31:49 -0700

    feat: reorganize embedded module exports and add IterableEmbeddedViewType enum

--- src/embedded/enums/index.ts
+++ src/embedded/enums/index.ts
@@ -1,11 +1,1 @@
-/**
- * The view type for an embedded message.
- */
-export enum IterableEmbeddedViewType {
-  /** The embedded view is a banner */
-  Banner = 0,
-  /** The embedded view is a card */
-  Card = 1,
-  /** The embedded view is a notification */
-  Notification = 2,
-}
+export * from './IterableEmbeddedViewType';
