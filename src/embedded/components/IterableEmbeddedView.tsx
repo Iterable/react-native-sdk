ba7fccc24e742cfdb5c5205c6040859bb4688681
Loren Posen <loren.posen@iterable.com>
2026-02-10 15:12:16 -0800

    docs: add JSDoc comments to IterableEmbeddedView component for better clarity on props and usage

--- src/embedded/components/IterableEmbeddedView.tsx
+++ src/embedded/components/IterableEmbeddedView.tsx
@@ -15,6 +15,17 @@ interface IterableEmbeddedViewProps extends IterableEmbeddedComponentProps {
   viewType: IterableEmbeddedViewType;
 }
 
+/**
+ *
+ * @param viewType - The type of view to render.
+ * @param message - The message to render.
+ * @param config - The config for the IterableEmbeddedView component, most likely used to style the view.
+ * @param onButtonClick - The function to call when a button is clicked.
+ * @returns The IterableEmbeddedView component.
+ *
+ * This component is used to render pre-created, customizable message displays
+ * included with Iterables RN SDK: cards, banners, and notifications.
+ */
 export const IterableEmbeddedView = ({
   viewType,
   ...props
