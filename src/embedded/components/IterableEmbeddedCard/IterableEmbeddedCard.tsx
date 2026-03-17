df4ae5ec57ac35dd200d7b6c698910278f91184a
Loren Posen <loren.posen@iterable.com>
2026-03-17 11:56:23 -0700

    feat: add IterableEmbeddedCard component with default export

--- src/embedded/components/IterableEmbeddedCard/IterableEmbeddedCard.tsx
+++ src/embedded/components/IterableEmbeddedCard/IterableEmbeddedCard.tsx
@@ -1,5 +1,5 @@
 import { View, Text } from 'react-native';
-import type { IterableEmbeddedComponentProps } from '../types/IterableEmbeddedComponentProps';
+import type { IterableEmbeddedComponentProps } from '../../types/IterableEmbeddedComponentProps';
 
 export const IterableEmbeddedCard = ({
   config,
